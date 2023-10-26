73036201549edf7e65270897a9fa5475be64bf37
yang <ywj9811@gmail.com>
2023-10-26 19:50:03 +0900

    RAC-83 feat : Controller 구현

--- src/main/java/com/postgraduate/domain/user/presentation/UserController.java
+++ src/main/java/com/postgraduate/domain/user/presentation/UserController.java
@@ -4,6 +4,8 @@ import com.postgraduate.domain.user.application.dto.res.UserInfoResponse;
 import com.postgraduate.domain.user.application.usecase.UserMyPageUseCase;
 import com.postgraduate.domain.user.presentation.constant.UserResponseMessage;
 import com.postgraduate.global.dto.ResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,10 +18,12 @@ import static org.springframework.http.HttpStatus.OK;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/user")
+@Tag(name = "USER Controller")
 public class UserController {
     private final UserMyPageUseCase myPageUseCase;
 
     @GetMapping("/me")
+    @Operation(description = "사용자 기본 정보 조회 - 닉네임, 은행, 계좌, 포인트")
     public ResponseDto<UserInfoResponse> getUserInfo() {
         UserInfoResponse userInfo = myPageUseCase.getUserInfo();
         return ResponseDto.create(OK.value(), GET_USER_INFO.getMessage(), userInfo);
