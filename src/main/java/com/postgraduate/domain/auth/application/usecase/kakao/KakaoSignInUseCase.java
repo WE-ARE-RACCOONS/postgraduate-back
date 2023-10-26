664a322aaf9c92cedd240f99e12a8f9d23deff43
ay-eonii <naayen@naver.com>
2023-10-26 23:08:41 +0900

    RAC-79 feat: 카카오 로그인 유스케이스 추가

---
+++ src/main/java/com/postgraduate/domain/auth/application/usecase/kakao/KakaoSignInUseCase.java
@@ -0,0 +1,24 @@
+package com.postgraduate.domain.auth.application.usecase.kakao;
+
+import com.postgraduate.domain.auth.presentation.dto.KakaoUserInfoResponse;
+import com.postgraduate.domain.user.domain.entity.User;
+import com.postgraduate.domain.user.domain.service.UserSaveService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class KakaoSignInUseCase {
+    private final KakaoAccessTokenUseCase kakaoTokenUseCase;
+    private final UserSaveService userSaveService;
+
+    @Transactional
+    public User getUser(String token) {
+        KakaoUserInfoResponse userInfo = kakaoTokenUseCase.getUserInfo(token);
+        Long socialId = userInfo.getId();
+        return userSaveService.saveUser(socialId);
+    }
+}
