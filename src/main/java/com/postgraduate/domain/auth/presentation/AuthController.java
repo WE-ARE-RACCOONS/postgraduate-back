36f5ee10a5bb16ca417c98547d8c51fc1a8221f4
ay-eonii <naayen@naver.com>
2023-10-26 23:09:18 +0900

    RAC-79 feat: 로그인 컨트롤러 추가

---
+++ src/main/java/com/postgraduate/domain/auth/presentation/AuthController.java
@@ -0,0 +1,21 @@
+package com.postgraduate.domain.auth.presentation;
+
+import com.postgraduate.domain.auth.application.usecase.kakao.KakaoSignInUseCase;
+import com.postgraduate.domain.auth.presentation.dto.KakaoLoginRequest;
+import com.postgraduate.domain.user.domain.entity.User;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+public class AuthController {
+    private final KakaoSignInUseCase kakaoSignInUseCase;
+
+    @PostMapping("/login")
+    public void getUserDetails(@RequestBody KakaoLoginRequest request) {
+        User user = kakaoSignInUseCase.getUser(request.getAccessToken());
+        //TODO: JWT 발급 + 신규 여부
+    }
+}
