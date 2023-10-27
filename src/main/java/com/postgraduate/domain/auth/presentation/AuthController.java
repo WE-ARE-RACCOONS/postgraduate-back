9f929a8f654a104dee39fbc1388a58045b3c091d
ay-eonii <naayen@naver.com>
2023-10-27 23:54:14 +0900

    RAC-85 feat: JWT 관련 presentation단 추가

--- src/main/java/com/postgraduate/domain/auth/presentation/AuthController.java
+++ src/main/java/com/postgraduate/domain/auth/presentation/AuthController.java
@@ -1,22 +1,46 @@
 package com.postgraduate.domain.auth.presentation;
 
+import com.postgraduate.domain.auth.application.dto.AuthUserResponse;
+import com.postgraduate.domain.auth.application.usecase.jwt.JwtUseCase;
 import com.postgraduate.domain.auth.application.usecase.kakao.KakaoSignInUseCase;
 import com.postgraduate.domain.auth.application.dto.JwtTokenResponse;
 import com.postgraduate.domain.auth.application.dto.KakaoLoginRequest;
-import com.postgraduate.domain.user.domain.entity.User;
+import com.postgraduate.global.dto.ResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
+
+import static com.postgraduate.domain.auth.presentation.contant.AuthResponseMessage.*;
+import static org.springframework.http.HttpStatus.*;
 
 @RestController
 @RequiredArgsConstructor
-@Slf4j
+@RequestMapping("/user")
+@Tag(name = "AUTH Controller")
 public class AuthController {
     private final KakaoSignInUseCase kakaoSignInUseCase;
+    private final JwtUseCase jwtUseCase;
 
     @PostMapping("/login")
-    public void getUserDetails(@RequestBody KakaoLoginRequest request) {
+    @Operation(description = "카카오 로그인")
+    public ResponseDto<JwtTokenResponse> getUserDetails(@RequestBody KakaoLoginRequest request, HttpServletResponse res) {
         AuthUserResponse authUser = kakaoSignInUseCase.getUser(request.getAccessToken());
-        //TODO: JWT 발급 + 신규 여부
+        JwtTokenResponse jwtToken = jwtUseCase.signIn(authUser.getUser(), authUser.isNew());
+        Cookie cookie = new Cookie("refreshToken", jwtToken.getRefreshToken());
+        cookie.setHttpOnly(true);
+        cookie.setMaxAge(60 * 60 * 24 * 30);
+        res.addCookie(cookie);
+        return ResponseDto.create(OK.value(), SUCCESS_AUTH_MESSAGE.getMessage(), jwtToken);
+    }
+
+    @PostMapping("/refresh")
+    @Operation(description = "refreshToken 으로 accessToken 재발급")
+    public ResponseDto<JwtTokenResponse> refresh(@Parameter(hidden = true) @CookieValue("refreshToken") String tokenInput) {
+        JwtTokenResponse jwtToken = jwtUseCase.regenerateToken(tokenInput);
+        return ResponseDto.create(OK.value(), SUCCESS_REGENERATE_TOKEN_MESSAGE.getMessage(), jwtToken);
     }
 }
