b288caa0f6a1aded7b3919dbd497db1934512f8a
ay-eonii <naayen@naver.com>
2023-10-27 14:55:00 +0900

    RAC-79 fix: `@Builder.Default`으로 USER 권한 초기화

--- src/main/java/com/postgraduate/domain/user/application/mapper/UserMapper.java
+++ src/main/java/com/postgraduate/domain/user/application/mapper/UserMapper.java
@@ -2,7 +2,6 @@ package com.postgraduate.domain.user.application.mapper;
 
 import com.postgraduate.domain.user.application.dto.res.UserInfoResponse;
 import com.postgraduate.domain.user.domain.entity.User;
-import com.postgraduate.domain.user.domain.entity.constant.Role;
 
 public class UserMapper {
     public static UserInfoResponse mapToInfo(User user) {
@@ -17,7 +16,6 @@ public class UserMapper {
     public static User mapToUser(Long socialId) {
         return User.builder()
                 .socialId(socialId)
-                .role(Role.USER)
                 .build();
     }
 }
--- src/main/java/com/postgraduate/domain/user/domain/entity/User.java
+++ src/main/java/com/postgraduate/domain/user/domain/entity/User.java
@@ -23,7 +23,7 @@ public class User {
     private Long userId;
 
     @Column(nullable = false, unique = true)
-    private Long socialId;
+    private String socialId;
 
     @Column(unique = true)
     private String email;
@@ -41,8 +41,8 @@ public class User {
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
-    @ColumnDefault("'USER'")
-    private Role role;
+    @Builder.Default
+    private Role role = Role.USER;
 
     @Column(nullable = false)
     @CreationTimestamp
