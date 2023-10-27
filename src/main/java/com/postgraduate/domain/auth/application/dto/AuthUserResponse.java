aa8b34cfe90af229c9296ae4232eb9debac8c2e3
ay-eonii <naayen@naver.com>
2023-10-27 23:43:15 +0900

    RAC-85 feat: `AuthUserResponse` 추가

---
+++ src/main/java/com/postgraduate/domain/auth/application/dto/AuthUserResponse.java
@@ -0,0 +1,16 @@
+package com.postgraduate.domain.auth.application.dto;
+
+import com.postgraduate.domain.user.domain.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthUserResponse {
+    private User user;
+    private boolean isNew;
+}
