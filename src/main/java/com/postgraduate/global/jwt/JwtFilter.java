641412a93ead268debcf1523b628b71152b57de0
ay-eonii <naayen@naver.com>
2023-10-27 23:51:06 +0900

    RAC-85 feat: JWT 설정 및 생성 로직 구현

---
+++ src/main/java/com/postgraduate/global/jwt/JwtFilter.java
@@ -0,0 +1,53 @@
+package com.postgraduate.global.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.List;
+
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+    private final JwtProvider jwtProvider;
+    private static final String AUTHORIZATION = "Authorization";
+    private static final String BEARER = "Bearer";
+    private static final List<String> IGNORE_JWT_FILTER_API = List.of(
+            "/user/login",
+            "/user/refresh",
+            "/admin/login",
+            "/admin/refresh"
+    );
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        if (IGNORE_JWT_FILTER_API.contains(request.getRequestURI())) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String token = resolveToken(request);
+        if (token != null) {
+            jwtProvider.validateToken(token);
+            Authentication authentication = this.jwtProvider.getAuthentication(token);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String resolveToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader(AUTHORIZATION);
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(BEARER)) {
+            return bearerToken.split(" ")[1];
+        }
+        return null;
+    }
+}
+
