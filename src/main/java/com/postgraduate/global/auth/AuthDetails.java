c747bfae742cdfb1b772610a0e2bb2366d68075d
ay-eonii <naayen@naver.com>
2023-10-27 23:49:59 +0900

    RAC-85 feat: UserDetails,UserDetailsService 커스텀

---
+++ src/main/java/com/postgraduate/global/auth/AuthDetails.java
@@ -0,0 +1,53 @@
+package com.postgraduate.global.auth;
+
+import com.postgraduate.domain.user.domain.entity.constant.Role;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@Getter
+@AllArgsConstructor
+public class AuthDetails implements UserDetails {
+    private final Long userId;
+    private final Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority(role.toString()));
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
