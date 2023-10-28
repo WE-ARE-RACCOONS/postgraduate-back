8ae91c3adb6ca5e88e25f4290f82699b37917e2d
ay-eonii <naayen@naver.com>
2023-10-29 02:06:25 +0900

    RAC-85 fix: `JwtFilter` 필요없는 로직 삭제

--- src/main/java/com/postgraduate/global/jwt/JwtFilter.java
+++ src/main/java/com/postgraduate/global/jwt/JwtFilter.java
@@ -11,23 +11,15 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-import java.util.List;
 
 @RequiredArgsConstructor
 public class JwtFilter extends OncePerRequestFilter {
     private final JwtProvider jwtProvider;
     private static final String AUTHORIZATION = "Authorization";
     private static final String BEARER = "Bearer";
-    private static final List<String> JWT_FILTER_API = List.of(
-    );
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        if (!JWT_FILTER_API.contains(request.getRequestURI())) {
-            filterChain.doFilter(request, response);
-            return;
-        }
-
         String token = resolveToken(request);
         if (token != null) {
             jwtProvider.validateToken(token);
