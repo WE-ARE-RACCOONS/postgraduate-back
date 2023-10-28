e9b871064523e300e5f6e83074931cf1a7795582
ay-eonii <naayen@naver.com>
2023-10-28 23:02:28 +0900

    RAC-85 fix: cookie 사용하지 않도록 변경

--- src/main/java/com/postgraduate/domain/auth/presentation/AuthController.java
+++ src/main/java/com/postgraduate/domain/auth/presentation/AuthController.java
@@ -5,13 +5,12 @@ import com.postgraduate.domain.auth.application.usecase.jwt.JwtUseCase;
 import com.postgraduate.domain.auth.application.usecase.kakao.KakaoSignInUseCase;
 import com.postgraduate.domain.auth.application.dto.JwtTokenResponse;
 import com.postgraduate.domain.auth.application.dto.KakaoLoginRequest;
+import com.postgraduate.global.auth.AuthDetails;
 import com.postgraduate.global.dto.ResponseDto;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import static com.postgraduate.domain.auth.presentation.contant.AuthResponseMessage.*;
@@ -27,20 +26,18 @@ public class AuthController {
 
     @PostMapping("/login")
     @Operation(description = "카카오 로그인")
-    public ResponseDto<JwtTokenResponse> getUserDetails(@RequestBody KakaoLoginRequest request, HttpServletResponse res) {
+    public ResponseDto<JwtTokenResponse> getUserDetails(@RequestBody KakaoLoginRequest request) {
         AuthUserResponse authUser = kakaoSignInUseCase.getUser(request.getAccessToken());
         JwtTokenResponse jwtToken = jwtUseCase.signIn(authUser.getUser(), authUser.isNew());
-        Cookie cookie = new Cookie("refreshToken", jwtToken.getRefreshToken());
-        cookie.setHttpOnly(true);
-        cookie.setMaxAge(60 * 60 * 24 * 30);
-        res.addCookie(cookie);
         return ResponseDto.create(OK.value(), SUCCESS_AUTH_MESSAGE.getMessage(), jwtToken);
     }
 
     @PostMapping("/refresh")
     @Operation(description = "refreshToken 으로 accessToken 재발급")
-    public ResponseDto<JwtTokenResponse> refresh(@Parameter(hidden = true) @CookieValue("refreshToken") String tokenInput) {
-        JwtTokenResponse jwtToken = jwtUseCase.regenerateToken(tokenInput);
+    public ResponseDto<JwtTokenResponse> refresh(@AuthenticationPrincipal AuthDetails authDetails) {
+        Long userId = authDetails.getUserId();
+        String refreshToken = "";/*Redis에서 userId로 refreshToken 확인*/
+        JwtTokenResponse jwtToken = jwtUseCase.regenerateToken(refreshToken);
         return ResponseDto.create(OK.value(), SUCCESS_REGENERATE_TOKEN_MESSAGE.getMessage(), jwtToken);
     }
 }
