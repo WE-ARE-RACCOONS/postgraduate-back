d5b1b47eae8d2882208191925c3595ce1e513680
yang <ywj9811@gmail.com>
2023-10-30 02:17:05 +0900

    RAC-83 fix : 리스트 조회 교수님 추가

--- src/main/java/com/postgraduate/domain/mentoring/application/mapper/MentoringMapper.java
+++ src/main/java/com/postgraduate/domain/mentoring/application/mapper/MentoringMapper.java
@@ -1,6 +1,6 @@
 package com.postgraduate.domain.mentoring.application.mapper;
 
-import com.postgraduate.domain.mentoring.application.dto.MentoringInfo;
+import com.postgraduate.domain.mentoring.application.dto.AppliedMentoringInfo;
 import com.postgraduate.domain.mentoring.domain.entity.Mentoring;
 import com.postgraduate.domain.senior.domain.entity.Senior;
 
@@ -8,26 +8,27 @@ import java.util.List;
 import java.util.stream.Stream;
 
 public class MentoringMapper {
-    public static MentoringInfo mapToExpectedInfo(Mentoring mentoring) {
+    public static AppliedMentoringInfo mapToExpectedInfo(Mentoring mentoring) {
         Senior senior = mentoring.getSenior();
         List<String> dates = Stream.of(mentoring.getDate())
                 .toList();
-        return MentoringInfo.builder()
+        return AppliedMentoringInfo.builder()
                 .mentoringId(mentoring.getMentoringId())
                 .dates(dates)
                 .seniorId(senior.getSeniorId())
                 .nickName(senior.getUser().getNickName())
                 .postgradu(senior.getPostgradu())
                 .lab(senior.getLab())
+                .professor(senior.getProfessor())
                 .field(senior.getField())
                 .chatLink(senior.getChatLink())
                 .build();
     }
-    public static MentoringInfo mapToWaitingOrDoneMentoringInfo(Mentoring mentoring) {
+    public static AppliedMentoringInfo mapToWaitingOrDoneMentoringInfo(Mentoring mentoring) {
         Senior senior = mentoring.getSenior();
         List<String> dates = Stream.of(mentoring.getDate())
                 .toList();
-        return MentoringInfo.builder()
+        return AppliedMentoringInfo.builder()
                 .mentoringId(mentoring.getMentoringId())
                 .dates(dates)
                 .term(senior.getTerm())
@@ -35,6 +36,7 @@ public class MentoringMapper {
                 .nickName(senior.getUser().getNickName())
                 .postgradu(senior.getPostgradu())
                 .lab(senior.getLab())
+                .professor(senior.getProfessor())
                 .field(senior.getField())
                 .build();
     }
