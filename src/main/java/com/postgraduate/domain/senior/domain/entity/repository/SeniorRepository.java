3212e4f6bd4b63ed2f91ee26bbcd5fddb5aee809
ay-eonii <naayen@naver.com>
2023-10-31 15:17:21 +0900

    RAC-81 feat: User로 Senior 찾는 기능

--- src/main/java/com/postgraduate/domain/senior/domain/entity/repository/SeniorRepository.java
+++ src/main/java/com/postgraduate/domain/senior/domain/entity/repository/SeniorRepository.java
@@ -1,8 +1,11 @@
 package com.postgraduate.domain.senior.domain.entity.repository;
 
 import com.postgraduate.domain.senior.domain.entity.Senior;
+import com.postgraduate.domain.user.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SeniorRepository extends JpaRepository<Senior, Long> {
+import java.util.Optional;
 
+public interface SeniorRepository extends JpaRepository<Senior, Long> {
+    Optional<Senior> findByUser(User user);
 }
