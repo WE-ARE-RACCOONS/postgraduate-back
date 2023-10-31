3e8afd3a4fef8aa638e06c4bbcff723848b228a0
ay-eonii <naayen@naver.com>
2023-10-31 14:18:43 +0900

    RAC-81 feat: Senior 생성 및 저장

---
+++ src/main/java/com/postgraduate/domain/senior/domain/entity/repository/SeniorRepository.java
@@ -0,0 +1,8 @@
+package com.postgraduate.domain.senior.domain.entity.repository;
+
+import com.postgraduate.domain.senior.domain.entity.Senior;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SeniorRepository extends JpaRepository<Senior, Long> {
+
+}
