a10f1da6acad07dbe5097caf8ba80d78d8e02cde
ay-eonii <naayen@naver.com>
2023-10-31 19:48:00 +0900

    RAC-81 style: Entity 공백 유지

--- src/main/java/com/postgraduate/domain/senior/domain/entity/Senior.java
+++ src/main/java/com/postgraduate/domain/senior/domain/entity/Senior.java
@@ -1,6 +1,7 @@
 package com.postgraduate.domain.senior.domain.entity;
 
 import com.postgraduate.domain.user.domain.entity.User;
+import com.postgraduate.domain.user.domain.entity.constant.Role;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -16,34 +17,52 @@ public class Senior {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long seniorId;
+
     @OneToOne
     private User user;
+
     @Column(nullable = false)
     private String college;
+
     @Column(nullable = false)
     private String major;
+
     @Column(nullable = false)
     private String postgradu;
+
     @Column(nullable = false)
     private String professor;
+
     @Column(nullable = false)
     private String lab;
+
     @Column(nullable = false)
     private String field;
+
     private String info;
+
     private String target;
+
     private String chatLink;
+
     private String time;
+
     private int term;
+
     @Column(nullable = false)
     private String account;
+
     @Column(nullable = false)
     private String bank;
+
     private String certification;
+
     @Column(nullable = false)
     private String rrn;
+
     @Column(nullable = false)
     private boolean status;
+
     @Column(nullable = false)
     private int hit;
 
