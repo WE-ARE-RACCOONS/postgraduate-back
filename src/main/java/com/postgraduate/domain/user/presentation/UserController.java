3ae8ce9e8d298afb6bb6986513ace0c490a31985
yang <ywj9811@gmail.com>
2023-10-31 18:03:30 +0900

    RAC-83 feat : 닉네임 중복 체크

--- src/main/java/com/postgraduate/domain/user/presentation/UserController.java
+++ src/main/java/com/postgraduate/domain/user/presentation/UserController.java
@@ -11,8 +11,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
-import static com.postgraduate.domain.user.presentation.constant.UserResponseMessage.GET_USER_INFO;
-import static com.postgraduate.domain.user.presentation.constant.UserResponseMessage.UPDATE_USER_INFO;
+import static com.postgraduate.domain.user.presentation.constant.UserResponseMessage.*;
 import static org.springframework.http.HttpStatus.OK;
 
 @RestController
@@ -23,7 +22,7 @@ public class UserController {
     private final UserMyPageUseCase myPageUseCase;
 
     @GetMapping("/me")
-    @Operation(description = "사용자 기본 정보 조회 - 닉네임, 은행, 계좌, 포인트")
+    @Operation(description = "사용자 기본 정보 조회 - 닉네임, 프로필, 포인트")
     public ResponseDto<UserInfoResponse> getUserInfo(@AuthenticationPrincipal AuthDetails authDetails) {
         UserInfoResponse userInfo = myPageUseCase.getUserInfo(authDetails);
         return ResponseDto.create(OK.value(), GET_USER_INFO.getMessage(), userInfo);
@@ -34,5 +33,12 @@ public class UserController {
     public ResponseDto updateNickName(@AuthenticationPrincipal AuthDetails authDetails, @RequestBody UserNickNameRequest userNickNameRequest) {
         myPageUseCase.updateUser(authDetails, userNickNameRequest.getNickName());
         return ResponseDto.create(OK.value(), UPDATE_USER_INFO.getMessage());
+    }
+
+    @GetMapping("/nickname")
+    @Operation(description = "사용자 닉네임 중복체크")
+    public ResponseDto<Boolean> duplicatedNickName(@RequestParam String nickName) {
+        boolean checkDup = myPageUseCase.duplicatedNickName(nickName);
+        return ResponseDto.create(OK.value(), GET_NICKNAME_CHECK.getMessage(), checkDup);
     }
 }
