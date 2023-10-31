9930628ebf16b0388fcfcb75f5dec43620f14e2c
ay-eonii <naayen@naver.com>
2023-10-31 14:15:07 +0900

    RAC-81 feat: 대학원생 회원가입 request

---
+++ src/main/java/com/postgraduate/domain/senior/application/dto/req/SeniorSignUpRequest.java
@@ -0,0 +1,32 @@
+package com.postgraduate.domain.senior.application.dto.req;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class SeniorSignUpRequest {
+    @NotNull
+    private String college;
+    @NotNull
+    private String major;
+    @NotNull
+    private String postgradu;
+    @NotNull
+    private String professor;
+    @NotNull
+    private String lab;
+    @NotNull
+    private String field;
+    @NotNull
+    private String certification;
+    @NotNull
+    private String account;
+    @NotNull
+    private String bank;
+    @NotNull
+    private String rrn;
+}
