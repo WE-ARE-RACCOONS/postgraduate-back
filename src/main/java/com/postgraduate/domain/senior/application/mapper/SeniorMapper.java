2502746b57c24581badcd3971afc9ec097a1e064
ay-eonii <naayen@naver.com>
2023-10-31 14:17:55 +0900

    RAC-81 feat: 회원가입 request, Senior 매퍼

---
+++ src/main/java/com/postgraduate/domain/senior/application/mapper/SeniorMapper.java
@@ -0,0 +1,24 @@
+package com.postgraduate.domain.senior.application.mapper;
+
+import com.postgraduate.domain.senior.application.dto.req.SeniorSignUpRequest;
+import com.postgraduate.domain.senior.domain.entity.Senior;
+import com.postgraduate.domain.user.domain.entity.User;
+
+public class SeniorMapper {
+
+    public static Senior mapToSenior(User user, SeniorSignUpRequest request) {
+        return Senior.builder()
+                .user(user)
+                .college(request.getCollege())
+                .major(request.getMajor())
+                .postgradu(request.getPostgradu())
+                //professor
+                .lab(request.getLab())
+                .field(request.getField())
+                .certification(request.getCertification())
+                //account
+                //bank
+                //rrn
+                .build();
+    }
+}
