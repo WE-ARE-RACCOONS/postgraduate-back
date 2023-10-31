f2b1dc417c9e40ef6a691bfca5b660bc434e5bfd
ay-eonii <naayen@naver.com>
2023-10-31 14:24:07 +0900

    RAC-81 feat: swagger jwt 인증

--- src/main/java/com/postgraduate/global/config/swagger/SwaggerConfig.java
+++ src/main/java/com/postgraduate/global/config/swagger/SwaggerConfig.java
@@ -2,6 +2,11 @@ package com.postgraduate.global.config.swagger;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import lombok.RequiredArgsConstructor;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
@@ -23,5 +28,22 @@ public class SwaggerConfig {
                 .group("PostGraduate API v1")
                 .pathsToMatch(paths)
                 .build();
+    }
+
+    @Bean
+    public OpenAPI openApi() {
+        String jwt = "JWT";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("JWT");
+        Components components = new Components().addSecuritySchemes(jwt, new SecurityScheme()
+                .name(jwt)
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+        );
+
+        return new OpenAPI()
+                .addServersItem(new Server().url("/"))
+                .addSecurityItem(securityRequirement)
+                .components(components);
     }
 }
