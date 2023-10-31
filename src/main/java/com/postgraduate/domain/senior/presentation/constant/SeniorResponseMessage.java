3d4ce5090f501c14e8c11d6b9aff94f6ce545656
ay-eonii <naayen@naver.com>
2023-10-31 15:20:45 +0900

    RAC-81 feat: Senior 프로필 등록 presentation

--- src/main/java/com/postgraduate/domain/senior/presentation/constant/SeniorResponseMessage.java
+++ src/main/java/com/postgraduate/domain/senior/presentation/constant/SeniorResponseMessage.java
@@ -6,7 +6,8 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum SeniorResponseMessage {
-    SUCCESS_SENIOR_SIGN_UP_MESSAGE("대학원생 가입에 성공하였습니다.");
+    SUCCESS_SENIOR_SIGN_UP_MESSAGE("대학원생 가입에 성공하였습니다."),
+    SUCCESS_UPDATE_PROFILE_MESSAGE("대학원생 프로필 등록에 성공하였습니다");
 
     private final String message;
 }
