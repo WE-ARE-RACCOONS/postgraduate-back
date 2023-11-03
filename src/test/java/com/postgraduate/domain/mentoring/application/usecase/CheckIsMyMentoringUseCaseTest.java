89cab11acb4e0b2f9635d81ecbc2ad8ccbc00f1d
ay-eonii <naayen@naver.com>
2023-11-03 20:32:59 +0900

    RAC-102 test: `@InjectMocks` 사용하도록 변경

--- src/test/java/com/postgraduate/domain/mentoring/application/usecase/CheckIsMyMentoringUseCaseTest.java
+++ src/test/java/com/postgraduate/domain/mentoring/application/usecase/CheckIsMyMentoringUseCaseTest.java
@@ -2,47 +2,36 @@ package com.postgraduate.domain.mentoring.application.usecase;
 
 import com.postgraduate.domain.mentoring.domain.entity.Mentoring;
 import com.postgraduate.domain.mentoring.domain.service.MentoringGetService;
-import com.postgraduate.domain.senior.domain.entity.Senior;
-import com.postgraduate.domain.senior.domain.service.SeniorGetService;
 import com.postgraduate.domain.user.domain.entity.User;
-import org.junit.jupiter.api.BeforeEach;
+import com.postgraduate.global.exception.ApplicationException;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class CheckIsMyMentoringUseCaseTest {
     @Mock
     private MentoringGetService mentoringGetService;
-    @Mock
-    private SeniorGetService seniorGetService;
 
+    @InjectMocks
     private CheckIsMyMentoringUseCase checkIsMyMentoringUseCase;
-
-    @BeforeEach
-    public void setup() {
-        MockitoAnnotations.openMocks(this);
-        checkIsMyMentoringUseCase = new CheckIsMyMentoringUseCase(
-                mentoringGetService,
-                seniorGetService
-        );
-    }
 
     @Test
     public void testCheckByRoleInvalid() {
         User user = new User();
         Long mentoringId = 1L;
         Mentoring mentoring = mock();
-        Senior senior = mock();
 
-        when(seniorGetService.byUser(user)).thenReturn(senior);
         when(mentoring.getUser()).thenReturn(new User());
         when(mentoringGetService.byMentoringId(mentoringId)).thenReturn(mentoring);
 
-        assertThrows(IllegalArgumentException.class, () -> {
+        assertThrows(ApplicationException.class, () -> {
             checkIsMyMentoringUseCase.checkByRole(user, mentoringId);
         });
     }
