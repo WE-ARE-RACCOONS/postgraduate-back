cbae7149d288d2ea90490b43baeb89b717bb2d56
ay-eonii <naayen@naver.com>
2023-11-03 14:20:18 +0900

    RAC-102 fix: 본인 멘토링 확인 로직 수정

--- src/main/java/com/postgraduate/domain/mentoring/application/usecase/CheckIsMyMentoringUseCase.java
+++ src/main/java/com/postgraduate/domain/mentoring/application/usecase/CheckIsMyMentoringUseCase.java
@@ -5,9 +5,6 @@ import com.postgraduate.domain.mentoring.domain.service.MentoringGetService;
 import com.postgraduate.domain.senior.domain.entity.Senior;
 import com.postgraduate.domain.senior.domain.service.SeniorGetService;
 import com.postgraduate.domain.user.domain.entity.User;
-import com.postgraduate.domain.user.domain.entity.constant.Role;
-import com.postgraduate.global.auth.AuthDetails;
-import com.postgraduate.global.config.security.util.SecurityUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,27 +14,13 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class CheckIsMyMentoringUseCase {
     private final MentoringGetService mentoringGetService;
-    private final SecurityUtils securityUtils;
     private final SeniorGetService seniorGetService;
-    public Mentoring checkByRole(AuthDetails authDetails, Long mentoringId) {
-        Role role = authDetails.getRole();
-        User user = securityUtils.getLoggedInUser(authDetails);
+    public Mentoring checkByRole(User user, Long mentoringId) {
         Mentoring mentoring = mentoringGetService.byMentoringId(mentoringId);
 
-        switch (role) {
-            case ADMIN -> {}
-            case USER -> {
-                if (mentoring.getUser() != user) {
-                    throw new IllegalArgumentException("대학생으로서 권한없음");
-                }
-            }
-            case SENIOR -> {
-                Senior senior = seniorGetService.byUser(user);
-                if (mentoring.getSenior() != senior) {
-                    throw new IllegalArgumentException("대학원생으로서 권한없음");
-                }
-            }
-            default -> throw new IllegalArgumentException("권한없음");
+        Senior senior = seniorGetService.byUser(user);
+        if (mentoring.getUser() != user && mentoring.getSenior() != senior) {
+            throw new IllegalArgumentException("권한없음");
         }
         return mentoring;
     }
--- src/main/java/com/postgraduate/domain/mentoring/application/usecase/MentoringUpdateUseCase.java
+++ src/main/java/com/postgraduate/domain/mentoring/application/usecase/MentoringUpdateUseCase.java
@@ -4,7 +4,9 @@ import com.postgraduate.domain.mentoring.application.dto.req.MentoringStatusRequ
 import com.postgraduate.domain.mentoring.domain.entity.Mentoring;
 import com.postgraduate.domain.mentoring.domain.entity.constant.Status;
 import com.postgraduate.domain.mentoring.domain.service.MentoringUpdateService;
+import com.postgraduate.domain.user.domain.entity.User;
 import com.postgraduate.global.auth.AuthDetails;
+import com.postgraduate.global.config.security.util.SecurityUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,11 +15,13 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 @RequiredArgsConstructor
 public class MentoringUpdateUseCase {
+    private final SecurityUtils securityUtils;
     private final MentoringUpdateService mentoringUpdateService;
     private final CheckIsMyMentoringUseCase checkIsMyMentoringUseCase;
 
     public void updateStatus(AuthDetails authDetails, Long mentoringId, MentoringStatusRequest request) {
-        Mentoring mentoring = checkIsMyMentoringUseCase.checkByRole(authDetails, mentoringId);
+        User user = securityUtils.getLoggedInUser(authDetails);
+        Mentoring mentoring = checkIsMyMentoringUseCase.checkByRole(user, mentoringId);
         mentoringUpdateService.updateStatus(mentoring, Status.valueOf(request.getStatus()));
     }
 }
