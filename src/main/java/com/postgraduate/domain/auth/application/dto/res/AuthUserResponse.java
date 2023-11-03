43c5935d2e57df744eb244476f7cc0c97b31ad68
yang <ywj9811@gmail.com>
2023-11-03 14:59:43 +0900

    RAC-100 fix : null사용 않도록 수정

--- src/main/java/com/postgraduate/domain/auth/application/dto/res/AuthUserResponse.java
+++ src/main/java/com/postgraduate/domain/auth/application/dto/res/AuthUserResponse.java
@@ -1,16 +1,19 @@
 package com.postgraduate.domain.auth.application.dto.res;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.postgraduate.domain.user.domain.entity.User;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.Optional;
+
 @Builder
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class AuthUserResponse {
-    private User user;
+    private Optional<User> user;
     private Long socialId;
 }
