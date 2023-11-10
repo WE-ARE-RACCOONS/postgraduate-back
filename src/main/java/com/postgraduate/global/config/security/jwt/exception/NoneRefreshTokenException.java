dce6174cc7376a0d61a16c28f709aad8d779c298
yang <ywj9811@gmail.com>
2023-11-10 17:52:23 +0900

    RAC-136 refactor : 변경 사항 반영

--- src/main/java/com/postgraduate/global/config/security/jwt/exception/NoneRefreshTokenException.java
+++ src/main/java/com/postgraduate/global/config/security/jwt/exception/NoneRefreshTokenException.java
@@ -1,10 +1,10 @@
 package com.postgraduate.global.config.security.jwt.exception;
 
-import com.postgraduate.global.config.security.jwt.constant.JwtResponseCode;
-import com.postgraduate.global.config.security.jwt.constant.JwtResponseMessage;
+import static com.postgraduate.global.config.security.jwt.constant.JwtResponseCode.NOT_FOUND_REFRESH_TOKEN;
+import static com.postgraduate.global.config.security.jwt.constant.JwtResponseMessage.NOT_FOUND_REFRESH;
 
 public class NoneRefreshTokenException extends JwtException {
     public NoneRefreshTokenException() {
-        super(JwtResponseMessage.NONE_REFRESH.getMessage(), JwtResponseCode.NONE_REFRESH_TOKEN.getCode());
+        super(NOT_FOUND_REFRESH.getMessage(), NOT_FOUND_REFRESH_TOKEN.getCode());
     }
 }
