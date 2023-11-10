7d8568e48f30352aa1eddb11b33acd659ad57796
ay-eonii <naayen@naver.com>
2023-11-10 16:34:39 +0900

    RAC-138 feat: 모든 결제 정보 색인 기능

---
+++ src/main/java/com/postgraduate/domain/payment/domain/service/PaymentGetService.java
@@ -0,0 +1,17 @@
+package com.postgraduate.domain.payment.domain.service;
+
+import com.postgraduate.domain.payment.domain.entity.Payment;
+import com.postgraduate.domain.payment.domain.repository.PaymentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class PaymentGetService {
+    private final PaymentRepository paymentRepository;
+    public List<Payment> all() {
+        return paymentRepository.findAll();
+    }
+}
