1e2d8bf23ec1e77989d650e109ed488f14813524
ay-eonii <naayen@naver.com>
2023-11-10 13:12:32 +0900

    RAC-138 feat: 대기중 상태인 선배 리스트

--- src/main/java/com/postgraduate/domain/senior/domain/service/SeniorGetService.java
+++ src/main/java/com/postgraduate/domain/senior/domain/service/SeniorGetService.java
@@ -1,10 +1,14 @@
 package com.postgraduate.domain.senior.domain.service;
 
 import com.postgraduate.domain.senior.domain.entity.Senior;
+import com.postgraduate.domain.senior.domain.entity.constant.Status;
 import com.postgraduate.domain.senior.domain.repository.SeniorRepository;
 import com.postgraduate.domain.user.domain.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -17,5 +21,9 @@ public class SeniorGetService {
 
     public Senior bySeniorId(Long seniorId) {
         return seniorRepository.findById(seniorId).orElseThrow(/**예외 처리**/);
+    }
+
+    public List<Senior> byStatus(Status status) {
+        return seniorRepository.findAllByStatus(status).orElse(new ArrayList<>());
     }
 }
