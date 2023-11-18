59a5a8b0c34714f667d8b15bb9cb5bde01f129db
ay-eonii <naayen@naver.com>
2023-11-18 19:23:51 +0900

    RAC-128 feat: 인가 예외 핸들러

---
+++ src/main/java/com/postgraduate/global/config/security/filter/CustomAccessDeniedHandler.java
@@ -0,0 +1,32 @@
+package com.postgraduate.global.config.security.filter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.postgraduate.global.dto.ErrorResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+import static com.postgraduate.domain.auth.presentation.contant.AuthResponseCode.AUTH_DENIED;
+import static com.postgraduate.domain.auth.presentation.contant.AuthResponseMessage.PERMISSION_DENIED;
+
+@Component
+@RequiredArgsConstructor
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+    private final ObjectMapper objectMapper;
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException {
+        response.setStatus(HttpStatus.FORBIDDEN.value());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        objectMapper.writeValue(
+                response.getOutputStream(),
+                new ErrorResponse(AUTH_DENIED.getCode(), PERMISSION_DENIED.getMessage())
+        );
+    }
+}
