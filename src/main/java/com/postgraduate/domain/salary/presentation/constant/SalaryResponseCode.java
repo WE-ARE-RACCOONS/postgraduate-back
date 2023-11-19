595f52202bf92dc608f87d48d5585df4d67f0d1b
ay-eonii <naayen@naver.com>
2023-11-19 23:20:30 +0900

    RAC-157 feat: salary 응답 코드 및 메시지

---
+++ src/main/java/com/postgraduate/domain/salary/presentation/constant/SalaryResponseCode.java
@@ -0,0 +1,16 @@
+package com.postgraduate.domain.salary.presentation.constant;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public enum SalaryResponseCode {
+    SALARY_FIND("SLR200"),
+    SALARY_UPDATE("SLR201"),
+    SALARY_CREATE("SLR202"),
+    SALARY_DELETE("SLR203"),
+
+    NONE_SALARY("EX500");
+    private final String code;
+}
