1045a995738f1fba2cbbd9b328be2d01514eebe4
ay-eonii <naayen@naver.com>
2023-11-19 13:33:43 +0900

    RAC-156 feat: 선배 상세조회 application

--- src/main/java/com/postgraduate/domain/senior/presentation/SeniorController.java
+++ src/main/java/com/postgraduate/domain/senior/presentation/SeniorController.java
@@ -4,7 +4,9 @@ import com.postgraduate.domain.senior.application.dto.req.SeniorAccountRequest;
 import com.postgraduate.domain.senior.application.dto.req.SeniorCertificationRequest;
 import com.postgraduate.domain.senior.application.dto.req.SeniorMyPageProfileRequest;
 import com.postgraduate.domain.senior.application.dto.req.SeniorProfileRequest;
+import com.postgraduate.domain.senior.application.dto.res.SeniorDetailResponse;
 import com.postgraduate.domain.senior.application.dto.res.SeniorInfoResponse;
+import com.postgraduate.domain.senior.application.usecase.SeniorInfoUseCase;
 import com.postgraduate.domain.senior.application.usecase.SeniorManageUseCase;
 import com.postgraduate.domain.senior.application.usecase.SeniorMyPageUseCase;
 import com.postgraduate.domain.user.domain.entity.User;
@@ -15,7 +17,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
-import static com.postgraduate.domain.senior.presentation.constant.SeniorResponseCode.*;
+import static com.postgraduate.domain.senior.presentation.constant.SeniorResponseCode.SENIOR_FIND;
+import static com.postgraduate.domain.senior.presentation.constant.SeniorResponseCode.SENIOR_UPDATE;
 import static com.postgraduate.domain.senior.presentation.constant.SeniorResponseMessage.*;
 
 
@@ -26,6 +29,7 @@ import static com.postgraduate.domain.senior.presentation.constant.SeniorRespons
 public class SeniorController {
     private final SeniorMyPageUseCase seniorMyPageUseCase;
     private final SeniorManageUseCase seniorManageUseCase;
+    private final SeniorInfoUseCase seniorInfoUseCase;
 
     @PatchMapping("/certification")
     @Operation(summary = "대학원생 인증", description = "이미지 업로드 이후 url 담아서 요청")
@@ -64,5 +68,12 @@ public class SeniorController {
                                         @RequestBody SeniorMyPageProfileRequest myPageProfileRequest) {
         seniorMyPageUseCase.updateSeniorMyPageProfile(user, myPageProfileRequest);
         return ResponseDto.create(SENIOR_UPDATE.getCode(), UPDATE_MYPAGE_PROFILE.getMessage());
+    }
+
+    @GetMapping("/{seniorId}")
+    @Operation(summary = "대학원생 상세 조회")
+    public ResponseDto<SeniorDetailResponse> getSeniorDetails(@PathVariable Long seniorId) {
+        SeniorDetailResponse seniorDetail = seniorInfoUseCase.getSeniorDetail(seniorId);
+        return ResponseDto.create(SENIOR_FIND.getCode(), GET_SENIOR_INFO.getMessage(), seniorDetail);
     }
 }
