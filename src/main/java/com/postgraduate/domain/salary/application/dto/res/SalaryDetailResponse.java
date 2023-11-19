8e775c38c6a7b54a45605ae093f497d1b570bffd
ay-eonii <naayen@naver.com>
2023-11-19 23:33:17 +0900

    RAC-157 feat: salary 상세 조회 response

---
+++ src/main/java/com/postgraduate/domain/salary/application/dto/res/SalaryDetailResponse.java
@@ -0,0 +1,19 @@
+package com.postgraduate.domain.salary.application.dto.res;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDate;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class SalaryDetailResponse {
+    private String profile;
+    private String nickName;
+    private String date;
+    private int term;
+    private int pay;
+    private LocalDate salaryDate;
+}
