ae2e95505392db113305d60893e99298c783c978
yang <ywj9811@gmail.com>
2023-11-24 02:48:55 +0900

    RAC-155 feat : 멘토 검색 response작성

---
+++ src/main/java/com/postgraduate/domain/senior/application/dto/res/SeniorSearchResponse.java
@@ -0,0 +1,20 @@
+package com.postgraduate.domain.senior.application.dto.res;
+
+import jakarta.validation.constraints.NotNull;
+
+public record SeniorSearchResponse(
+        @NotNull
+        String profile,
+        @NotNull
+        String nickName,
+        @NotNull
+        String postgradu,
+        @NotNull
+        String major,
+        @NotNull
+        String lab,
+        @NotNull
+        String oneLiner,
+        @NotNull
+        String[] keyword
+) {}
