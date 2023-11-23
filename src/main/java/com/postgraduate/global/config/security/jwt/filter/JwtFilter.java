fb1d858159d2d7646223b9705645a2cae11ea929
yang <ywj9811@gmail.com>
2023-11-23 19:16:12 +0900

    RAC-169 refactor : 인증 정보 로그 처리(통신 오류 추정 위해)

--- src/main/java/com/postgraduate/global/config/security/jwt/filter/JwtFilter.java
+++ src/main/java/com/postgraduate/global/config/security/jwt/filter/JwtFilter.java
@@ -1,11 +1,13 @@
-package com.postgraduate.global.config.security.jwt;
+package com.postgraduate.global.config.security.jwt.filter;
 
+import com.postgraduate.global.config.security.jwt.JwtProvider;
 import com.postgraduate.global.exception.ApplicationException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.util.StringUtils;
@@ -14,6 +16,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 
 @RequiredArgsConstructor
+@Slf4j
 public class JwtFilter extends OncePerRequestFilter {
     private final JwtProvider jwtProvider;
     private final String AUTHORIZATION = "Authorization";
@@ -23,11 +26,13 @@ public class JwtFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String token = resolveToken(request);
         if (token != null) {
+            log.info("토큰 함께 요청 : {}", token);
             if (!jwtProvider.validateToken(response, token))
                 return;
             try {
                 Authentication authentication = jwtProvider.getAuthentication(response, token);
                 SecurityContextHolder.getContext().setAuthentication(authentication);
+                log.info("context 인증 정보 저장 : {}", authentication.getName());
             } catch (ApplicationException ex) {
                 return;
             }
