2461ba866c8b0fd0867ea74fd15ed3ff027db1e1
ay-eonii <naayen@naver.com>
2023-12-05 16:42:42 +0900

    RAC-186 feat: 정산완료일 필드 추가 salaryDate(정산예정일), salaryDoneDate(정산완료일)

--- src/main/java/com/postgraduate/domain/salary/domain/entity/Salary.java
+++ src/main/java/com/postgraduate/domain/salary/domain/entity/Salary.java
@@ -9,6 +9,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Entity
 @Builder
@@ -32,4 +33,7 @@ public class Salary {
 
     @Column(nullable = false)
     private LocalDate salaryDate;
+
+    @Column
+    private LocalDateTime salaryDoneDate;
 }
