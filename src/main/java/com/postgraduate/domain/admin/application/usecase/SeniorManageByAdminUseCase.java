0f0172dac2ac3f2cc5977b3431d878355bd63cca
ay-eonii <naayen@naver.com>
2023-12-09 01:35:50 +0900

    RAC-186 fix: 선배 인증 상태 변경 로직 수정

--- src/main/java/com/postgraduate/domain/admin/application/usecase/SeniorManageByAdminUseCase.java
+++ src/main/java/com/postgraduate/domain/admin/application/usecase/SeniorManageByAdminUseCase.java
@@ -1,8 +1,8 @@
 package com.postgraduate.domain.admin.application.usecase;
 
+import com.postgraduate.domain.admin.application.dto.SeniorInfo;
 import com.postgraduate.domain.admin.application.dto.req.SeniorStatusRequest;
 import com.postgraduate.domain.admin.application.dto.res.CertificationDetailsResponse;
-import com.postgraduate.domain.admin.application.dto.SeniorInfo;
 import com.postgraduate.domain.admin.application.dto.res.SeniorManageResponse;
 import com.postgraduate.domain.admin.application.mapper.AdminMapper;
 import com.postgraduate.domain.admin.exception.SeniorNotWaitingException;
@@ -12,9 +12,7 @@ import com.postgraduate.domain.salary.domain.service.SalaryGetService;
 import com.postgraduate.domain.senior.domain.entity.Senior;
 import com.postgraduate.domain.senior.domain.entity.constant.Status;
 import com.postgraduate.domain.senior.domain.service.SeniorGetService;
-import com.postgraduate.domain.user.domain.entity.User;
-import com.postgraduate.domain.user.domain.entity.constant.Role;
-import com.postgraduate.domain.user.domain.service.UserUpdateService;
+import com.postgraduate.domain.senior.domain.service.SeniorUpdateService;
 import com.postgraduate.domain.wish.domain.entity.Wish;
 import com.postgraduate.domain.wish.domain.service.WishGetService;
 import lombok.RequiredArgsConstructor;
@@ -32,7 +30,7 @@ import static com.postgraduate.domain.salary.util.SalaryUtil.getStatus;
 @RequiredArgsConstructor
 public class SeniorManageByAdminUseCase {
     private final SeniorGetService seniorGetService;
-    private final UserUpdateService userUpdateService;
+    private final SeniorUpdateService seniorUpdateService;
     private final SalaryGetService salaryGetService;
     private final WishGetService wishGetService;
 
@@ -46,11 +44,7 @@ public class SeniorManageByAdminUseCase {
 
     public void updateSeniorStatus(Long seniorId, SeniorStatusRequest request) {
         Senior senior = seniorGetService.bySeniorId(seniorId);
-        senior.updateStatus(request.getStatus());
-        if (request.getStatus() == Status.APPROVE) {
-            User user = senior.getUser();
-            userUpdateService.updateRole(user.getUserId(), Role.SENIOR);
-        }
+        seniorUpdateService.updateCertificationStatus(senior, request.getStatus());
     }
 
     public SeniorManageResponse getSeniors() {
