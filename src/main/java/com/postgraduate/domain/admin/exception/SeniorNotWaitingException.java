d2c6bb38cdc7943a08d86445313ab04d9a9e41d3
ay-eonii <naayen@naver.com>
2023-12-09 23:26:44 +0900

    RAC-186 feat: 승인대기 아닌 선배 응답코드 반영

--- src/main/java/com/postgraduate/domain/admin/exception/SeniorNotWaitingException.java
+++ src/main/java/com/postgraduate/domain/admin/exception/SeniorNotWaitingException.java
@@ -1,10 +1,10 @@
 package com.postgraduate.domain.admin.exception;
 
-import static com.postgraduate.domain.senior.presentation.constant.SeniorResponseCode.NONE_SENIOR;
-import static com.postgraduate.domain.senior.presentation.constant.SeniorResponseMessage.NOT_WAITING_STATUS;
+import com.postgraduate.domain.senior.presentation.constant.SeniorResponseCode;
+import com.postgraduate.domain.senior.presentation.constant.SeniorResponseMessage;
 
 public class SeniorNotWaitingException extends SeniorException{
     public SeniorNotWaitingException() {
-        super(NOT_WAITING_STATUS.getMessage(), NONE_SENIOR.getCode());
+        super(SeniorResponseMessage.NOT_WAITING_STATUS.getMessage(), SeniorResponseCode.NOT_WAITING_STATUS.getCode());
     }
 }
