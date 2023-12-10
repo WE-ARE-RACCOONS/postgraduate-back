91a382d57bc3d3484b2896a630852f2267e86e4e
ay-eonii <naayen@naver.com>
2023-12-10 22:34:04 +0900

    RAC-187 fix: 관리자 응답코드 분리

---
+++ src/main/java/com/postgraduate/domain/admin/presentation/constant/AdminResponseMessage.java
@@ -0,0 +1,15 @@
+package com.postgraduate.domain.admin.presentation.constant;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum AdminResponseMessage {
+    GET_DETAILS("상세 조회에 성공하였습니다."),
+    GET_LIST("목록 조회에 성공하였습니다."),
+    UPDATE_SENIOR_STATUS("선배 승인 상태 수정에 성공하였습니다."),
+    UPDATE_SALARY_STATUS("정산 상태 수정에 성공하였습니다.");
+
+    private final String message;
+}
