a452411a2ecc02eb021cad88210daee4dc7a599d
yang <ywj9811@gmail.com>
2023-12-12 22:44:09 +0900

    RAC-230 feat : 탈퇴 컨트롤러 작성

--- src/main/java/com/postgraduate/domain/auth/presentation/constant/AuthResponseMessage.java
+++ src/main/java/com/postgraduate/domain/auth/presentation/constant/AuthResponseMessage.java
@@ -10,6 +10,7 @@ public enum AuthResponseMessage {
     NOT_REGISTERED_USER("가입하지 않은 유저입니다."),
     SUCCESS_REGENERATE_TOKEN("토큰 재발급에 성공하였습니다."),
     LOGOUT_USER("로그아웃에 성공하였습니다."),
+    SIGNOUT_USER("회원탈퇴에 성공하였습니다."),
 
     PERMISSION_DENIED("권한이 없습니다."),
     KAKAO_INVALID("카카오 정보가 유효하지 않습니다."),
