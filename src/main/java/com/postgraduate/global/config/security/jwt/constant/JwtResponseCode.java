e3aa458e7e164588111cf9b3323664550c78ebff
yang <ywj9811@gmail.com>
2023-12-13 02:56:45 +0900

    RAC-231 feat : 탈퇴 회원 토큰 발급x

--- src/main/java/com/postgraduate/global/config/security/jwt/constant/JwtResponseCode.java
+++ src/main/java/com/postgraduate/global/config/security/jwt/constant/JwtResponseCode.java
@@ -9,7 +9,8 @@ public enum JwtResponseCode {
     EXPIRED_JWT("EX200"),
     INVALID_JWT("EX201"),
     NOT_FOUND_REFRESH_TOKEN("EX202"),
-    INVALID_REFRESH_TOKEN("EX202");
+    INVALID_REFRESH_TOKEN("EX202"),
+    DELETE_USER("EX203");
 
     private final String code;
 }
