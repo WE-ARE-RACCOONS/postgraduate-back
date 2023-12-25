34e74f0aa5165096cd69d3f59f9b0cbd941a6adc
yang <ywj9811@gmail.com>
2023-12-26 02:28:38 +0900

    RAC-240 fix : mentoring 자동 취소 처리시 LocalDateTime 수정

--- src/main/java/com/postgraduate/domain/mentoring/domain/repository/MentoringRepository.java
+++ src/main/java/com/postgraduate/domain/mentoring/domain/repository/MentoringRepository.java
@@ -7,6 +7,7 @@ import com.postgraduate.domain.user.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,7 +15,7 @@ public interface MentoringRepository extends JpaRepository<Mentoring, Long> {
     Optional<Mentoring> findByMentoringIdAndUser_IsDeleteAndSenior_User_IsDelete(Long mentoringId, Boolean isUserDelete, Boolean isSeniorDelete);
     List<Mentoring> findAllByUserAndStatus(User user, Status status);
     List<Mentoring> findAllBySeniorAndStatus(Senior senior, Status status);
-    List<Mentoring> findAllByStatusAndCreatedAtIsBefore(Status status, LocalDate now);
+    List<Mentoring> findAllByStatusAndCreatedAtIsBefore(Status status, LocalDateTime now);
     List<Mentoring> findAllByUser_UserId(Long userId);
     List<Mentoring> findAllBySenior_SeniorId(Long seniorId);
 }
