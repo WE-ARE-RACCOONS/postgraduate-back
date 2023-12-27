63cab3cf4d7fe86599f28aadba357b25256959c6
yang <ywj9811@gmail.com>
2023-12-28 03:01:02 +0900

    RAC-250 fix : 기존 키워드 6개 초과시 저장되는 오류 수정

--- src/main/java/com/postgraduate/domain/auth/application/usecase/oauth/SignUpUseCase.java
+++ src/main/java/com/postgraduate/domain/auth/application/usecase/oauth/SignUpUseCase.java
@@ -5,6 +5,7 @@ import com.postgraduate.domain.auth.application.dto.req.SeniorSignUpRequest;
 import com.postgraduate.domain.auth.application.dto.req.SignUpRequest;
 import com.postgraduate.domain.auth.application.dto.req.UserChangeRequest;
 import com.postgraduate.domain.senior.application.mapper.SeniorMapper;
+import com.postgraduate.domain.senior.application.utils.SeniorUtils;
 import com.postgraduate.domain.senior.domain.entity.Senior;
 import com.postgraduate.domain.senior.domain.service.SeniorSaveService;
 import com.postgraduate.domain.user.application.mapper.UserMapper;
@@ -31,6 +32,7 @@ public class SignUpUseCase {
     private final WishSaveService wishSaveService;
     private final SeniorSaveService seniorSaveService;
     private final UserUtils userUtils;
+    private final SeniorUtils seniorUtils;
 
     public User userSignUp(SignUpRequest request) {
         userUtils.checkPhoneNumber(request.phoneNumber());
@@ -42,6 +44,7 @@ public class SignUpUseCase {
     }
 
     public User seniorSignUp(SeniorSignUpRequest request) {
+        seniorUtils.checkKeyword(request.keyword());
         userUtils.checkPhoneNumber(request.phoneNumber());
         User user = UserMapper.mapToUser(request);
         userSaveService.saveUser(user);
@@ -51,6 +54,7 @@ public class SignUpUseCase {
     }
 
     public User changeSenior(User user, SeniorChangeRequest changeRequest) {
+        seniorUtils.checkKeyword(changeRequest.keyword());
         Senior senior = SeniorMapper.mapToSenior(user, changeRequest); //todo : 예외 처리
         seniorSaveService.saveSenior(senior);
         user = userGetService.getUser(user.getUserId());
