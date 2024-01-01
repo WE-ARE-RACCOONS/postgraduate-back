6685bf58798802cd6365189aa5cc0df4588caf7b
yang <ywj9811@gmail.com>
2024-01-02 01:02:11 +0900

    RAC-264 refactor : 선배 자신의 정산 목록 조회 성능 개선

--- src/main/java/com/postgraduate/domain/salary/domain/repository/SalaryDslRepository.java
+++ src/main/java/com/postgraduate/domain/salary/domain/repository/SalaryDslRepository.java
@@ -1,9 +1,15 @@
 package com.postgraduate.domain.salary.domain.repository;
 
 import com.postgraduate.domain.salary.application.dto.SeniorSalary;
+import com.postgraduate.domain.salary.domain.entity.Salary;
+import com.postgraduate.domain.senior.domain.entity.Senior;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.util.List;
+
 public interface SalaryDslRepository {
     Page<SeniorSalary> findDistinctBySearchSenior(String search, Pageable pageable);
+    //todo : 관리자 정산 목록 쿼리 조회 수정
+    List<Salary> findAllBySeniorIdAndStatus(Senior senior, Boolean status);
 }
