797f8efd2060937013411ee524725fcfd87a063b
ay-eonii <naayen@naver.com>
2024-01-10 17:43:27 +0900

    RAC-254 test: @Disabled 추가

--- src/test/java/com/postgraduate/IntegrationTest.java
+++ src/test/java/com/postgraduate/IntegrationTest.java
@@ -1,6 +1,7 @@
 package com.postgraduate;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,6 +18,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
+@Disabled
 public class IntegrationTest {
 
     @Autowired
