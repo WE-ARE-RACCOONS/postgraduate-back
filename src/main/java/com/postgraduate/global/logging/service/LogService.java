185c7242d05ae53b768f2cca07320240d02496cd
yang <ywj9811@gmail.com>
2024-01-14 01:55:41 +0900

    RAC-277 feat : Timeout설정 및 서버연결 예외 처리

--- src/main/java/com/postgraduate/global/logging/service/LogService.java
+++ src/main/java/com/postgraduate/global/logging/service/LogService.java
@@ -1,20 +1,28 @@
 package com.postgraduate.global.logging.service;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.postgraduate.global.logging.dto.LogRequest;
 import com.postgraduate.global.mq.producer.MessageProducer;
+import com.postgraduate.global.slack.SlackMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class LogService {
     private final MessageProducer messageProducer;
+    private final SlackMessage slackMessage;
 
-    public void save(LogRequest logRequest) throws JsonProcessingException {
-        log.info("log save");
-        messageProducer.sendMessage(logRequest);
+    public void save(LogRequest logRequest) throws IOException {
+        try {
+            log.info("log save");
+            messageProducer.sendMessage(logRequest);
+        } catch (Exception ex) {
+            log.error("로그 서버 연결 실패");
+            slackMessage.sendSlackLog(ex);
+        }
     }
 }
