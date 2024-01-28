0f243dc45a3c8a37a42c85d5f4c49b226815a6cd
ay-eonii <naayen@naver.com>
2024-01-28 15:21:05 +0900

    RAC-286 fix: 유저 response 통일

--- src/main/java/com/postgraduate/domain/user/exception/DeletedUserException.java
+++ src/main/java/com/postgraduate/domain/user/exception/DeletedUserException.java
@@ -3,8 +3,8 @@ package com.postgraduate.domain.user.exception;
 import com.postgraduate.domain.user.presentation.constant.UserResponseCode;
 import com.postgraduate.domain.user.presentation.constant.UserResponseMessage;
 
-public class DeletedUserException extends UserException{
+public class DeletedUserException extends UserException {
     public DeletedUserException() {
-        super(UserResponseMessage.DELETED_USER.getMessage(), UserResponseCode.DELETED_USER.getCode());
+        super(UserResponseMessage.DELETED_USER.getMessage(), UserResponseCode.USER_DELETED.getCode());
     }
 }
