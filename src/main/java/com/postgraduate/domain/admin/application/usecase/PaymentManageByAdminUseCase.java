ee6e65717c5268b7cc1c1dd1b40d643e1cbfddb4
yang <ywj9811@gmail.com>
2024-02-06 02:10:58 +0900

    RAC-308 refactor : Null pointers should not be dereferenced 해결

--- src/main/java/com/postgraduate/domain/admin/application/usecase/PaymentManageByAdminUseCase.java
+++ src/main/java/com/postgraduate/domain/admin/application/usecase/PaymentManageByAdminUseCase.java
@@ -8,7 +8,6 @@ import com.postgraduate.domain.admin.application.mapper.AdminMapper;
 import com.postgraduate.domain.mentoring.domain.entity.Mentoring;
 import com.postgraduate.domain.mentoring.domain.service.MentoringGetService;
 import com.postgraduate.domain.payment.domain.entity.Payment;
-import com.postgraduate.domain.payment.domain.service.PaymentGetService;
 import com.postgraduate.domain.payment.domain.service.PaymentUpdateService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -21,6 +20,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Optional;
 
 import static com.postgraduate.domain.payment.domain.entity.constant.Status.CANCEL;
 import static org.springframework.http.CacheControl.noCache;
@@ -29,23 +29,22 @@ import static org.springframework.http.CacheControl.noCache;
 @Transactional
 @RequiredArgsConstructor
 public class PaymentManageByAdminUseCase {
+    private static final String SUCCESS = "0000";
+    private static final String CST_ID = "test";
+    private static final String CUST_KEY = "abcd1234567890";
+    private static final String REDIRECT_URL = "http://localhost:8080";
+    private static final String REFUND_KEY = "a41ce010ede9fcbfb3be86b24858806596a9db68b79d138b147c3e563e1829a0";
+    private static final String FLAG = "Y";
+    private static final String REFUND_URL = "https://democpay.payple.kr";
+    private static final String CERTIFICATION_URL = "https://democpay.payple.kr/php/auth.php";
+
     private final MentoringGetService mentoringGetService;
     private final PaymentUpdateService paymentUpdateService;
     private final WebClient webClient;
 
-    private String CST_ID = "test"; //todo : 나중에 @Value사용하도록 변경
-    private String CUST_KEY = "abcd1234567890";//todo : 나중에 @Value사용하도록 변경
-    private String REDIRECT_URL = "http://localhost:8080"; //todo : 마찬가지
-    private String REFUND_KEY = "a41ce010ede9fcbfb3be86b24858806596a9db68b79d138b147c3e563e1829a0";
-    private final static String FLAG = "Y";
-    private final static String REFUND_URL = "https://democpay.payple.kr";
-    private final static String CERTIFICATION_URL = "https://democpay.payple.kr/php/auth.php";
-
     public PaymentManageResponse getPayments(Integer page, String search) {
         Page<Mentoring> mentorings = mentoringGetService.all(page, search);
-        List<PaymentInfo> paymentInfos = mentorings.stream()
-                .map(AdminMapper::mapToPaymentInfo)
-                .toList();
+        List<PaymentInfo> paymentInfos = mentorings.map(AdminMapper::mapToPaymentInfo).toList();
         long totalElements = mentorings.getTotalElements();
         int totalPages = mentorings.getTotalPages();
         return new PaymentManageResponse(paymentInfos, totalElements, totalPages);
@@ -54,26 +53,29 @@ public class PaymentManageByAdminUseCase {
     public void refundPayment(Long mentoringId) {
         Mentoring mentoring = mentoringGetService.byMentoringId(mentoringId);
         Payment payment = mentoring.getPayment();
-        CertificationResponse certificationResponse = getCertificationResponse();
-        getRefundResponse(certificationResponse, payment);
+        CertificationResponse certificationResponse = getCertificationResponse().orElseThrow();
+        RefundResponse response = getRefundResponse(certificationResponse, payment).orElseThrow();
+        if (!response.PCD_PAY_CODE().equals(SUCCESS))
+            throw new IllegalArgumentException();
         paymentUpdateService.updateStatus(payment, CANCEL);
     }
 
-    private RefundResponse getRefundResponse(CertificationResponse response, Payment payment) {
-        return webClient.post()
+    private Optional<RefundResponse> getRefundResponse(CertificationResponse response, Payment payment) {
+        return Optional.ofNullable(webClient.post()
                 .uri(response.PCD_PAY_URL())
-                .headers(h -> h.setContentType(MediaType.APPLICATION_JSON))
-                .headers(h -> h.setCacheControl(noCache()))
-                .headers(h -> h.set("referer", REDIRECT_URL))
+                .headers(h -> {
+                    h.setContentType(MediaType.APPLICATION_JSON);
+                    h.setCacheControl(noCache());
+                    h.set("referer", REDIRECT_URL);
+                })
                 .bodyValue(getRefundRequestBody(response, payment))
                 .retrieve()
                 .bodyToMono(RefundResponse.class)
-                .block();
+                .block());
     }
 
     private MultiValueMap<String, String> getRefundRequestBody(CertificationResponse response, Payment payment) {
-        String paidAt = payment.getPaidAt()
-                .format(DateTimeFormatter.ofPattern("yyyyMMdd"));
+        String paidAt = payment.getPaidAt().format(DateTimeFormatter.ofPattern("yyyyMMdd"));
         MultiValueMap<String, String> requestBody = new LinkedMultiValueMap<>();
         requestBody.add("PCD_CST_ID", response.cst_id());
         requestBody.add("PCD_CUST_KEY", response.custKey());
@@ -86,15 +88,17 @@ public class PaymentManageByAdminUseCase {
         return requestBody;
     }
 
-    private CertificationResponse getCertificationResponse() {
-        return webClient.post()
+    private Optional<CertificationResponse> getCertificationResponse() {
+        return Optional.ofNullable(webClient.post()
                 .uri(CERTIFICATION_URL)
-                .headers(h -> h.setContentType(MediaType.APPLICATION_JSON))
-                .headers(h -> h.set("referer", REDIRECT_URL))
+                .headers(h -> {
+                    h.setContentType(MediaType.APPLICATION_JSON);
+                    h.set("referer", REDIRECT_URL);
+                })
                 .bodyValue(getCertificationRequestBody())
                 .retrieve()
                 .bodyToMono(CertificationResponse.class)
-                .block();
+                .block());
     }
 
     private MultiValueMap<String, String> getCertificationRequestBody() {
@@ -105,3 +109,4 @@ public class PaymentManageByAdminUseCase {
         return requestBody;
     }
 }
+//todo : 환불 로직 수정 필요
