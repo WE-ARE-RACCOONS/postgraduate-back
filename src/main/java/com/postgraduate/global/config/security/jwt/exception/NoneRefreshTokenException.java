7f7b51ee417fac0463aacbe292a52dcd655f7219
yang <ywj9811@gmail.com>
2024-02-06 04:09:32 +0900

    RAC-308 refactor : Inheritance tree of classes should not be too deep 해결

--- src/main/java/com/postgraduate/global/config/security/jwt/exception/NoneRefreshTokenException.java
+++ src/main/java/com/postgraduate/global/config/security/jwt/exception/NoneRefreshTokenException.java
@@ -1,9 +1,11 @@
 package com.postgraduate.global.config.security.jwt.exception;
 
+import com.postgraduate.global.exception.ApplicationException;
+
 import static com.postgraduate.global.config.security.jwt.constant.JwtResponseCode.NOT_FOUND_REFRESH_TOKEN;
 import static com.postgraduate.global.config.security.jwt.constant.JwtResponseMessage.NOT_FOUND_REFRESH;
 
-public class NoneRefreshTokenException extends JwtException {
+public class NoneRefreshTokenException extends ApplicationException {
     public NoneRefreshTokenException() {
         super(NOT_FOUND_REFRESH.getMessage(), NOT_FOUND_REFRESH_TOKEN.getCode());
     }
