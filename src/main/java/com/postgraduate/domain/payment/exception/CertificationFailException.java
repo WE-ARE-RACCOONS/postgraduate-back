1a18a72d565adb59b012004e2411f9eb0c085d98
yang <ywj9811@gmail.com>
2024-02-13 03:09:54 +0900

    RAC-276 style : 주석 제거

--- src/main/java/com/postgraduate/domain/payment/exception/CertificationFailException.java
+++ src/main/java/com/postgraduate/domain/payment/exception/CertificationFailException.java
@@ -3,9 +3,7 @@ package com.postgraduate.domain.payment.exception;
 import com.postgraduate.global.exception.ApplicationException;
 
 import static com.postgraduate.domain.payment.presentation.constant.PaymentResponseCode.CERTIFICATION_DENIED;
-import static com.postgraduate.domain.payment.presentation.constant.PaymentResponseCode.PAYMENT_NOT_FOUND;
 import static com.postgraduate.domain.payment.presentation.constant.PaymentResponseMessage.DENIED_CERTIFICATION;
-import static com.postgraduate.domain.payment.presentation.constant.PaymentResponseMessage.NOT_FOUND_PAYMENT;
 
 
 public class CertificationFailException extends ApplicationException {
