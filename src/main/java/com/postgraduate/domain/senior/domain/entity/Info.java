9ffcb063fe67c21fd9367e9ab1fb0b8365624d68
yang <ywj9811@gmail.com>
2024-02-26 00:08:41 +0900

    RAC-304 fix : senior 프로필 업데이트 오류 수정

--- src/main/java/com/postgraduate/domain/senior/domain/entity/Info.java
+++ src/main/java/com/postgraduate/domain/senior/domain/entity/Info.java
@@ -1,12 +1,15 @@
 package com.postgraduate.domain.senior.domain.entity;
 
 import com.postgraduate.domain.senior.application.dto.req.SeniorMyPageProfileRequest;
+import com.postgraduate.domain.senior.domain.entity.constant.Field;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.Set;
 
 @Getter
 @AllArgsConstructor
@@ -44,9 +47,18 @@ public class Info {
     private String totalInfo; // 모든 Info정보 String으로 가지는 컬럼 - 검색시 사용
 
     public void updateMyPage(SeniorMyPageProfileRequest request) {
+        String[] fileds = request.field().split(",");
+        Set<String> fieldNames = Field.fieldNames();
         this.keyword = request.keyword();
         this.lab = request.lab();
         this.field = request.field();
+        for (String newField : fileds) {
+            if (fieldNames.contains(newField)) {
+                this.etcField = false;
+                return;
+            }
+            this.etcField = true;
+        }
         combineTotalInfo();
     }
 
