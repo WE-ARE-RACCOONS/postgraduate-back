861d04cb4089a6083dab2a09b6998c75da094b98
yang <ywj9811@gmail.com>
2024-03-02 16:33:29 +0900

    RAC-304 fix : 결제 실패시 환불 진행 추가

--- src/main/java/com/postgraduate/domain/payment/application/mapper/PaymentMapper.java
+++ src/main/java/com/postgraduate/domain/payment/application/mapper/PaymentMapper.java
@@ -25,4 +25,17 @@ public class PaymentMapper {
                 .paidAt(LocalDateTime.parse(request.PCD_PAY_TIME(), formatter))
                 .build();
     }
+
+    public static Payment resultToPayment(PaymentResultRequest request) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
+        return Payment.builder()
+                .salary(null)
+                .user(null)
+                .pay(Integer.parseInt(request.PCD_PAY_TOTAL()))
+                .orderId(request.PCD_PAY_OID())
+                .cardAuthNumber(request.PCD_PAY_CARDAUTHNO())
+                .cardReceipt(request.PCD_PAY_CARDRECEIPT())
+                .paidAt(LocalDateTime.parse(request.PCD_PAY_TIME(), formatter))
+                .build();
+    }
 }
