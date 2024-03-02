ca660c1f64e29da2dcac350eeff7e02e789c8955
양원준 <97458548+ywj9811@users.noreply.github.com>
2024-03-03 01:30:55 +0900

    RAC-304 test : 자동 갱신 확인

--- src/main/java/com/postgraduate/domain/mentoring/application/usecase/MentoringManageUseCase.java
+++ src/main/java/com/postgraduate/domain/mentoring/application/usecase/MentoringManageUseCase.java
@@ -130,7 +130,7 @@ public class MentoringManageUseCase {
         mentoringDeleteService.deleteMentoring(mentoring);
     }
 
-    @Scheduled(cron = "0 17 05 * * *", zone = "Asia/Seoul")
+    @Scheduled(cron = "0 33 01 * * *", zone = "Asia/Seoul")
     public void updateAutoCancel() {
         LocalDateTime now = LocalDateTime.now()
                 .toLocalDate()
