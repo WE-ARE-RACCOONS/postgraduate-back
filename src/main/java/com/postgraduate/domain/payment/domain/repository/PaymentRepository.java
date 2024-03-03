738140d8bbeda00439cdf99e15b2429fb51be353
yang <ywj9811@gmail.com>
2024-03-04 03:21:05 +0900

    RAC-304 fix : Payment Status DONE 조회하도록 수정

--- src/main/java/com/postgraduate/domain/payment/domain/repository/PaymentRepository.java
+++ src/main/java/com/postgraduate/domain/payment/domain/repository/PaymentRepository.java
@@ -1,6 +1,7 @@
 package com.postgraduate.domain.payment.domain.repository;
 
 import com.postgraduate.domain.payment.domain.entity.Payment;
+import com.postgraduate.domain.payment.domain.entity.constant.Status;
 import com.postgraduate.domain.senior.domain.entity.Senior;
 import com.postgraduate.domain.user.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,6 +9,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface PaymentRepository extends JpaRepository<Payment, Long>, PaymentDslRepository {
-    Optional<Payment> findByUserAndOrderId(User user, String orderId);
-    Optional<Payment> findBySalary_SeniorAndOrderId(Senior senior, String orderId);
+    Optional<Payment> findByUserAndOrderIdAndStatus(User user, String orderId, Status status);
+    Optional<Payment> findBySalary_SeniorAndOrderIdAndStatus(Senior senior, String orderId, Status status);
 }
