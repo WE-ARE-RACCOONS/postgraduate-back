2396ead7e1a341d849ea11a335f440097b432473
yang <ywj9811@gmail.com>
2024-03-10 23:14:11 +0900

    RAC-304 fix : Payple결제 취소시 리다이렉트 수정

--- src/main/java/com/postgraduate/domain/payment/presentation/PaymentController.java
+++ src/main/java/com/postgraduate/domain/payment/presentation/PaymentController.java
@@ -2,6 +2,7 @@ package com.postgraduate.domain.payment.presentation;
 
 import com.postgraduate.domain.payment.application.dto.req.PaymentResultRequest;
 import com.postgraduate.domain.payment.application.usecase.PaymentManageUseCase;
+import com.postgraduate.domain.senior.application.usecase.SeniorInfoUseCase;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -18,21 +19,34 @@ import java.io.IOException;
 @Tag(name = "PAYMENT Controller", description = "")
 public class PaymentController {
     private final PaymentManageUseCase paymentManageUseCase;
+    private final SeniorInfoUseCase seniorInfoUseCase;
     @Value("${payple.redirect-uri}")
     private String redirectUri;
     @Value("${payple.redirect-uri-dev}")
     private String redirectUriDev;
+    @Value("${payple.cancel-redirect-uri}")
+    private String cancelRedirectUri;
+    @Value("${payple.cancel-redirect-uri-dev}")
+    private String cancelRedirectUriDev;
 
     @PostMapping("/payple/result")
     public void resultGet(HttpServletResponse response, @ModelAttribute PaymentResultRequest request) throws IOException {
-        paymentManageUseCase.savePay(request);
-        response.sendRedirect(redirectUri + request.PCD_PAY_OID());
+        if (paymentManageUseCase.savePay(request)) {
+            response.sendRedirect(redirectUri + request.PCD_PAY_OID());
+            return;
+        }
+        Long seniorId = seniorInfoUseCase.getSeniorId(request.PCD_PAY_GOODS());
+        response.sendRedirect(cancelRedirectUri + seniorId);
     }
 
     @PostMapping("/payple/dev/result")
     public void resultGetWithDev(HttpServletResponse response, @ModelAttribute PaymentResultRequest request) throws IOException {
-        paymentManageUseCase.savePay(request);
-        response.sendRedirect(redirectUriDev + request.PCD_PAY_OID());
+        if (paymentManageUseCase.savePay(request)) {
+            response.sendRedirect(redirectUriDev + request.PCD_PAY_OID());
+            return;
+        }
+        Long seniorId = seniorInfoUseCase.getSeniorId(request.PCD_PAY_GOODS());
+        response.sendRedirect(cancelRedirectUriDev + seniorId);
     }
 
     @PostMapping("/webhook")
