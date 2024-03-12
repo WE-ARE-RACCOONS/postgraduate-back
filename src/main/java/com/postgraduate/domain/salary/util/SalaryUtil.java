2408b8799b7847c92f594bc2531c788fb6d8d1c5
yang <ywj9811@gmail.com>
2024-03-12 19:53:40 +0900

    RAC-322 fix : 정산일 계산 로직 수정

--- src/main/java/com/postgraduate/domain/salary/util/SalaryUtil.java
+++ src/main/java/com/postgraduate/domain/salary/util/SalaryUtil.java
@@ -2,15 +2,19 @@ package com.postgraduate.domain.salary.util;
 
 import com.postgraduate.domain.admin.presentation.constant.SalaryStatus;
 import com.postgraduate.domain.salary.domain.entity.Salary;
+import lombok.extern.slf4j.Slf4j;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 
 import static com.postgraduate.domain.admin.presentation.constant.SalaryStatus.*;
 
+@Slf4j
 public class SalaryUtil {
     private static final int SALARY_DATE = 4; // 목요일
     private static final int SALARY_END_DATE = 7; // 일요일
+    private static final int ONE_WEEK = 7;
+    private static final int ZERO = 0;
 
     private SalaryUtil() {
         throw new IllegalArgumentException();
@@ -20,12 +24,12 @@ public class SalaryUtil {
         LocalDate now = LocalDate.now();
         DayOfWeek dayOfWeek = now.getDayOfWeek();
         return dayOfWeek.getValue() == SALARY_END_DATE
-                ? now.plusDays(7 + (SALARY_DATE - dayOfWeek.getValue()))
-                : now.plusDays(7 + SALARY_DATE);
+                ? now.plusDays(ONE_WEEK + SALARY_DATE)
+                : now.plusDays(ONE_WEEK + (SALARY_DATE - dayOfWeek.getValue()));
     }
 
     public static SalaryStatus getStatus(Salary salary) {
-        if (salary.getTotalAmount() == 0) {
+        if (salary.getTotalAmount() == ZERO) {
             return NONE;
         }
         if (salary.status())
