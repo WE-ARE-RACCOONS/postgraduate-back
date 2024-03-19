e6ae306f37417b5b5cfe0c4c5650f5b339e92d89
yang <ywj9811@gmail.com>
2024-03-20 05:01:12 +0900

    RAC-326 refactor : 정산 리팩토

--- src/main/java/com/postgraduate/domain/salary/application/usecase/SalaryManageUseCase.java
+++ src/main/java/com/postgraduate/domain/salary/application/usecase/SalaryManageUseCase.java
@@ -1,28 +1,26 @@
 package com.postgraduate.domain.salary.application.usecase;
 
 import com.postgraduate.domain.salary.application.dto.SeniorAndAccount;
-import com.postgraduate.domain.salary.application.mapper.SalaryMapper;
 import com.postgraduate.domain.salary.domain.entity.Salary;
 import com.postgraduate.domain.salary.domain.service.SalaryGetService;
-import com.postgraduate.domain.salary.domain.service.SalarySaveService;
 import com.postgraduate.domain.senior.domain.service.SeniorGetService;
 import com.postgraduate.global.slack.SlackSalaryMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.util.List;
 
-@Transactional
+import static com.postgraduate.domain.salary.util.SalaryUtil.getSalaryDate;
+
 @Service
 @RequiredArgsConstructor
 public class SalaryManageUseCase {
-    private final SalarySaveService salarySaveService;
     private final SalaryGetService salaryGetService;
     private final SeniorGetService seniorGetService;
     private final SlackSalaryMessage slackSalaryMessage;
+    private final SalaryRenewalUseCase salaryRenewalUseCase;
 
     @Scheduled(cron = "0 0 0 * * 4", zone = "Asia/Seoul")
     public void createSalary() {
@@ -30,10 +28,9 @@ public class SalaryManageUseCase {
         slackSalaryMessage.sendSlackSalary(salaries);
 
         List<SeniorAndAccount> seniorAndAccounts = seniorGetService.findAllSeniorAndAccount();
-        LocalDate salaryDate = LocalDate.now().plusDays(14);
-        seniorAndAccounts.forEach(seniorAndAccount -> {
-            Salary salary = SalaryMapper.mapToSalary(seniorAndAccount.senior(), salaryDate, seniorAndAccount.account());
-            salarySaveService.save(salary);
-        });
+        LocalDate salaryDate = getSalaryDate().plusDays(7);
+        seniorAndAccounts.forEach(
+                seniorAndAccount -> salaryRenewalUseCase.createSalaryWithAuto(seniorAndAccount, salaryDate)
+        );
     }
 }
