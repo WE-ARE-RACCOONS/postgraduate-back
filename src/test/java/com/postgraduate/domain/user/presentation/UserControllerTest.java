d99e53d9d954923d5047a63d6c49d0c7ad2e0e07
yang <ywj9811@gmail.com>
2024-04-15 05:17:07 +0900

    RAC-343 test : UserController 단위 테스트 생성

--- src/test/java/com/postgraduate/domain/user/presentation/UserControllerTest.java
+++ src/test/java/com/postgraduate/domain/user/presentation/UserControllerTest.java
@@ -1,93 +1,85 @@
 package com.postgraduate.domain.user.presentation;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.postgraduate.IntegrationTest;
+import com.postgraduate.support.ControllerTest;
 import com.postgraduate.domain.user.application.dto.req.UserInfoRequest;
+import com.postgraduate.domain.user.application.dto.res.UserInfoResponse;
+import com.postgraduate.domain.user.application.dto.res.UserMyPageResponse;
+import com.postgraduate.domain.user.application.dto.res.UserPossibleResponse;
 import com.postgraduate.domain.user.domain.entity.User;
-import com.postgraduate.domain.user.domain.entity.constant.Role;
-import com.postgraduate.domain.user.domain.repository.UserRepository;
+import com.postgraduate.domain.user.exception.PhoneNumberException;
 import com.postgraduate.domain.user.presentation.constant.UserResponseCode;
-import com.postgraduate.global.config.security.jwt.util.JwtUtils;
-import com.postgraduate.global.slack.SlackLogErrorMessage;
-import com.postgraduate.global.slack.SlackSalaryMessage;
-import org.junit.jupiter.api.BeforeEach;
+import com.postgraduate.domain.user.presentation.constant.UserResponseMessage;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 
-import java.io.IOException;
-
-import static com.postgraduate.domain.user.presentation.constant.UserResponseCode.USER_FIND;
-import static com.postgraduate.domain.user.presentation.constant.UserResponseCode.USER_UPDATE;
+import static com.postgraduate.domain.user.presentation.constant.UserResponseCode.*;
 import static com.postgraduate.domain.user.presentation.constant.UserResponseMessage.*;
 import static java.time.LocalDateTime.now;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doNothing;
+import static org.mockito.BDDMockito.*;
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-class UserControllerTest extends IntegrationTest {
-    private static final String AUTHORIZATION = "Authorization";
-    private static final String BEARER = "Bearer ";
-    @Autowired
-    private ObjectMapper objectMapper;
-    @Autowired
-    private JwtUtils jwtUtil;
-    @Autowired
-    private UserRepository userRepository;
-    @MockBean
-    private SlackLogErrorMessage slackLogErrorMessage;
-    private String token;
+class UserControllerTest extends ControllerTest {
+    private static final String BEARER = "Bearer token";
 
-    @BeforeEach
-    void setUp() throws IOException {
-        User user = new User(0L, 1L, "mail", "후배", "011", "profile", 0, Role.USER, true, now(), now(), false);
-        userRepository.save(user);
-
-        token = jwtUtil.generateAccessToken(user.getUserId(), Role.USER);
-
-        doNothing().when(slackLogErrorMessage).sendSlackLog(any());
-    }
-
+    User user= resource.getUser();
     @Test
     @DisplayName("유저 마이페이지 정보를 조회한다")
+    @WithMockUser
     void getUserInfo() throws Exception {
+        given(userMyPageUseCase.getUserInfo(any()))
+                .willReturn(new UserMyPageResponse(user.getNickName(), user.getProfile()));
+
         mvc.perform(get("/user/me")
-                        .header(AUTHORIZATION, BEARER + token))
+                        .header(AUTHORIZATION, BEARER))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.code").value(USER_FIND.getCode()))
                 .andExpect(jsonPath("$.message").value(GET_USER_INFO.getMessage()))
-                .andExpect(jsonPath("$.data.nickName").isNotEmpty())
-                .andExpect(jsonPath("$.data.profile").isNotEmpty());
+                .andExpect(jsonPath("$.data.nickName").value(user.getNickName()))
+                .andExpect(jsonPath("$.data.profile").value(user.getProfile()));
     }
 
     @Test
     @DisplayName("유저 마이페이지 수정 전 기본 정보를 조회한다")
+    @WithMockUser
     void getOriginUserInfo() throws Exception {
+        UserInfoResponse userInfoResponse = new UserInfoResponse(user.getProfile(), user.getNickName(), user.getPhoneNumber());
+        given(userMyPageUseCase.getUserOriginInfo(any()))
+                .willReturn(userInfoResponse);
         mvc.perform(get("/user/me/info")
-                        .header(AUTHORIZATION, BEARER + token))
+                        .header(HttpHeaders.AUTHORIZATION, BEARER))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.code").value(USER_FIND.getCode()))
                 .andExpect(jsonPath("$.message").value(GET_USER_INFO.getMessage()))
-                .andExpect(jsonPath("$.data.profile").isNotEmpty())
-                .andExpect(jsonPath("$.data.nickName").isNotEmpty())
-                .andExpect(jsonPath("$.data.phoneNumber").isNotEmpty());
+                .andExpect(jsonPath("$.data.profile").value(userInfoResponse.profile()))
+                .andExpect(jsonPath("$.data.nickName").value(userInfoResponse.nickName()))
+                .andExpect(jsonPath("$.data.phoneNumber").value(userInfoResponse.phoneNumber()));
     }
 
     @Test
     @DisplayName("유저 마이페이지 정보를 수정한다.")
+    @WithMockUser
     void updateInfo() throws Exception {
         String request = objectMapper.writeValueAsString(
                 new UserInfoRequest("new_profile", "new후배", "01012345667")
         );
+
+        willDoNothing().given(userManageUseCase)
+                .updateInfo(any(), any());
+
         mvc.perform(patch("/user/me/info")
-                        .header(AUTHORIZATION, BEARER + token)
+                        .with(csrf())
+                        .header(HttpHeaders.AUTHORIZATION, BEARER )
                         .content(request)
                         .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaType.APPLICATION_JSON))
@@ -96,54 +88,62 @@ class UserControllerTest extends IntegrationTest {
                 .andExpect(jsonPath("$.message").value(UPDATE_USER_INFO.getMessage()));
     }
 
-    @ParameterizedTest
-    @ValueSource(strings = {"0101234567", "번호를한글열하나글자로"})
+    @Test
     @DisplayName("잘못된 번호로 수정할 수 없다")
-    void updateInvalidPhoneNumber(String phoneNumber) throws Exception {
+    @WithMockUser
+    void updateInvalidPhoneNumber() throws Exception {
+        UserInfoRequest userInfoRequest = new UserInfoRequest("new_profile", "new후배", "phoneNumber");
         String request = objectMapper.writeValueAsString(
-                new UserInfoRequest("new_profile", "new후배", phoneNumber)
+                userInfoRequest
         );
+
+        willThrow(new PhoneNumberException())
+                .given(userManageUseCase)
+                .updateInfo(any(), any());
+
         mvc.perform(patch("/user/me/info")
-                        .header(AUTHORIZATION, BEARER + token)
+                        .with(csrf())
+                        .header(HttpHeaders.AUTHORIZATION, BEARER )
                         .content(request)
                         .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.code").value(UserResponseCode.INVALID_PHONE_NUMBER.getCode()))
-                .andExpect(jsonPath("$.message").value(INVALID_PHONE_NUMBER.getMessage()));
+                .andExpect(jsonPath("$.message").value(UserResponseMessage.INVALID_PHONE_NUMBER.getMessage()));
     }
 
-    @Test
-    @DisplayName("선배 전환 가능 여부를 확인한다")
-    void checkRole() throws Exception {
+    @ParameterizedTest
+    @ValueSource(booleans = {true, false})
+    @WithMockUser
+    @DisplayName("선배 전환 가능 여부 테스트")
+    void checkRoleTrue(boolean tf) throws Exception {
+        UserPossibleResponse possibleResponse = new UserPossibleResponse(tf, user.getSocialId());
+
+        given(userMyPageUseCase.checkSenior(any()))
+                .willReturn(possibleResponse);
+
         mvc.perform(get("/user/me/role")
-                        .header(AUTHORIZATION, BEARER + token))
+                        .header(HttpHeaders.AUTHORIZATION, BEARER))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.code").value(USER_FIND.getCode()))
                 .andExpect(jsonPath("$.message").value(GET_SENIOR_CHECK.getMessage()))
-                .andExpect(jsonPath("$.data.possible").isNotEmpty())
-                .andExpect(jsonPath("$.data.socialId").isNotEmpty());
+                .andExpect(jsonPath("$.data.possible").value(possibleResponse.possible()))
+                .andExpect(jsonPath("$.data.socialId").value(possibleResponse.socialId()));
     }
 
-    @Test
+    @ParameterizedTest
+    @ValueSource(booleans = {true, false})
     @DisplayName("사용 가능한 닉네임 중복체크를 한다")
-    void duplicatedPossibleNickName() throws Exception {
+    @WithMockUser
+    void duplicatedPossibleNickNameWithTrue(boolean tf) throws Exception {
+        given(userManageUseCase.duplicatedNickName(any()))
+                .willReturn(tf);
+
         mvc.perform(get("/user/nickname")
                         .param("nickName", "new후배"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.code").value(USER_FIND.getCode()))
                 .andExpect(jsonPath("$.message").value(GET_NICKNAME_CHECK.getMessage()))
-                .andExpect(jsonPath("$.data").value(true));
-    }
-
-    @Test
-    @DisplayName("사용 불가능한 닉네임 중복체크를 한다")
-    void duplicatedImpossibleNickName() throws Exception {
-        mvc.perform(get("/user/nickname")
-                        .param("nickName", "후배"))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.code").value(USER_FIND.getCode()))
-                .andExpect(jsonPath("$.message").value(GET_NICKNAME_CHECK.getMessage()))
-                .andExpect(jsonPath("$.data").value(false));
+                .andExpect(jsonPath("$.data").value(tf));
     }
 }
