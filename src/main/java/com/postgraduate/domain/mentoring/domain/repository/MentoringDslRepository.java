070635da744be4ada9f151d696984ff770772eec
yang <ywj9811@gmail.com>
2024-07-02 01:33:16 +0900

    RAC-402 refactor : 불필요한 코드 삭제

--- src/main/java/com/postgraduate/domain/mentoring/domain/repository/MentoringDslRepository.java
+++ src/main/java/com/postgraduate/domain/mentoring/domain/repository/MentoringDslRepository.java
@@ -4,10 +4,7 @@ import com.postgraduate.domain.mentoring.domain.entity.Mentoring;
 import com.postgraduate.domain.mentoring.domain.entity.constant.Status;
 import com.postgraduate.domain.senior.domain.entity.Senior;
 import com.postgraduate.domain.user.domain.entity.User;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -18,9 +15,6 @@ public interface MentoringDslRepository {
     List<Mentoring> findAllByUserAndStatus(User user, Status status);
     Optional<Mentoring> findByMentoringId(Long mentoringId);
     List<Mentoring> findAllBySeniorAndSalaryStatus(Senior senior, Boolean status);
-    Page<Mentoring> findAllBySearchPayment(String search, Pageable pageable);
-    List<Mentoring> findAllByStatusAndCreatedAtIsBefore(Status status, LocalDateTime now);
-    List<Mentoring> findAllByStatus(Status status);
     Optional<Mentoring> findByMentoringIdAndUserForDetails(Long mentoringId, User user);
     Optional<Mentoring> findByMentoringIdAndSeniorForDetails(Long mentoringId, Senior senior);
 
