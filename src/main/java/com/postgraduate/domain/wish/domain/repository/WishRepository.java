1c4b88b3774b330a9683efb2c080384e9d7f0252
yang <ywj9811@gmail.com>
2024-07-02 01:42:58 +0900

    RAC-402 fix : 후배회원 불필요한 코드 삭제 및 탈퇴관련 로직 수정

--- src/main/java/com/postgraduate/domain/wish/domain/repository/WishRepository.java
+++ src/main/java/com/postgraduate/domain/wish/domain/repository/WishRepository.java
@@ -7,8 +7,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface WishRepository extends JpaRepository<Wish, Long>, WishDslRepository {
-    Optional<Wish> findByUser(User user);
-    Optional<Wish> findByMatchingReceiveIsTrueAndUser_UserId(Long userId);
+    Optional<Wish> findByUserAndUser_IsDeleteIsFalse(User user);
+    Optional<Wish> findByMatchingReceiveIsTrueAndUser_UserIdAndUser_IsDeleteIsFalse(Long userId);
 
-    Optional<Wish> findByWishIdAndMatchingReceiveIsTrue(Long wishId);
+    Optional<Wish> findByWishIdAndMatchingReceiveIsTrueAndUser_IsDeleteIsFalse(Long wishId);
 }
