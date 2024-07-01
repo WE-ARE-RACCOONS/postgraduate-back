436d958063a4fc4e24d06c268add59dc59a81a97
yang <ywj9811@gmail.com>
2024-07-02 01:42:16 +0900

    RAC-402 fix : 기본 회원관련 탈퇴 관련 로직 수정

--- src/main/java/com/postgraduate/domain/user/domain/repository/UserRepository.java
+++ src/main/java/com/postgraduate/domain/user/domain/repository/UserRepository.java
@@ -3,10 +3,12 @@ package com.postgraduate.domain.user.domain.repository;
 import com.postgraduate.domain.user.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findBySocialId(Long socialId);
+    List<User> findAllByIsDeleteIsFalse();
+    Optional<User> findBySocialIdAndIsDeleteIsFalse(Long socialId);
     Optional<User> findByNickName(String nickName);
     Optional<User> findByNickNameAndPhoneNumber(String nickName, String phoneNumber);
 }
