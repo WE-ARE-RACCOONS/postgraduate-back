ed5058d797f6b4ab6e23cd69894903b509821f53
yang <ywj9811@gmail.com>
2024-07-09 00:34:16 +0900

    RAC-405 feat : 대표 학교 추가

--- src/main/java/com/postgraduate/domain/senior/domain/entity/constant/Postgradu.java
+++ src/main/java/com/postgraduate/domain/senior/domain/entity/constant/Postgradu.java
@@ -1,25 +1,22 @@
 package com.postgraduate.domain.senior.domain.entity.constant;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
 import java.util.HashSet;
 import java.util.Set;
 
+@RequiredArgsConstructor
+@Getter
 public enum Postgradu {
-    SEOUL("서울대학교"), YONSEI("연세대학교");
+    SEOUL("서울대학교"), YONSEI("연세대학교"), KOREA("고려대학교"), KAIST("카이스트");
 
     private final String name;
-
-    public String getPostgradu() {
-        return name;
-    }
-
-    Postgradu(String field) {
-        this.name = field;
-    }
 
     public static Set<String> postgraduNames() {
         HashSet<String> postgraduNames = new HashSet<>();
         for (Postgradu postgradu : Postgradu.values()) {
-            postgraduNames.add(postgradu.getPostgradu());
+            postgraduNames.add(postgradu.getName());
         }
         return postgraduNames;
     }
