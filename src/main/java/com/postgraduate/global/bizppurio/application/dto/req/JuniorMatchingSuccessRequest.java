ceec5b4bb3a050748df436ef143f14f969cf371d
yang <ywj9811@gmail.com>
2024-07-09 00:45:24 +0900

    RAC-405 fix : 후배 매칭 성공 알림톡 내용 수정

--- src/main/java/com/postgraduate/global/bizppurio/application/dto/req/JuniorMatchingSuccessRequest.java
+++ src/main/java/com/postgraduate/global/bizppurio/application/dto/req/JuniorMatchingSuccessRequest.java
@@ -1,6 +1,6 @@
 package com.postgraduate.global.bizppurio.application.dto.req;
 
 public record JuniorMatchingSuccessRequest(
-        String phoneNumber, String name, String postgraduate, String major
+        String phoneNumber, String name, String postgraduate, String lab
 ) {
 }
