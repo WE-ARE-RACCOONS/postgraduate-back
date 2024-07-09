26adcb767e61fc3596336e16738c4e3530093063
yang <ywj9811@gmail.com>
2024-07-10 02:03:22 +0900

    RAC-410 fix : 응답 객체 변경

--- src/main/java/com/postgraduate/domain/salary/presentation/SalaryController.java
+++ src/main/java/com/postgraduate/domain/salary/presentation/SalaryController.java
@@ -8,6 +8,7 @@ import com.postgraduate.global.dto.ResponseDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import static com.postgraduate.domain.salary.presentation.constant.SalaryResponseCode.SALARY_FIND;
 import static com.postgraduate.domain.salary.presentation.constant.SalaryResponseMessage.GET_SALARY_INFO;
 import static com.postgraduate.domain.salary.presentation.constant.SalaryResponseMessage.GET_SALARY_LIST_INFO;
+import static com.postgraduate.global.dto.ResponseDto.create;
 
 @RestController
 @RequiredArgsConstructor
@@ -26,22 +28,22 @@ public class SalaryController {
 
     @GetMapping()
     @Operation(summary = "대학원생 정산 예정액, 다음 정산 예정일 조회", description = "마이페이지에서 공통으로 보이는 정산 예정 정보입니다.")
-    public ResponseDto<SalaryInfoResponse> getSalary(@AuthenticationPrincipal User user) {
+    public ResponseEntity<ResponseDto<SalaryInfoResponse>> getSalary(@AuthenticationPrincipal User user) {
         SalaryInfoResponse salary = salaryInfoUseCase.getSalary(user);
-        return ResponseDto.create(SALARY_FIND.getCode(), GET_SALARY_INFO.getMessage(), salary);
+        return ResponseEntity.ok(create(SALARY_FIND.getCode(), GET_SALARY_INFO.getMessage(), salary));
     }
 
     @GetMapping("/waiting")
     @Operation(summary = "대학원생 정산예정 목록 조회", description = "정산 예정 탭에서 보이는 목록입니다.")
-    public ResponseDto<SalaryDetailsResponse> getWaitingSalary(@AuthenticationPrincipal User user) {
+    public ResponseEntity<ResponseDto<SalaryDetailsResponse>> getWaitingSalary(@AuthenticationPrincipal User user) {
         SalaryDetailsResponse salary = salaryInfoUseCase.getSalaryDetail(user, false);
-        return ResponseDto.create(SALARY_FIND.getCode(), GET_SALARY_LIST_INFO.getMessage(), salary);
+        return ResponseEntity.ok(create(SALARY_FIND.getCode(), GET_SALARY_LIST_INFO.getMessage(), salary));
     }
 
     @GetMapping("/done")
     @Operation(summary = "대학원생 정산완료 목록 조회", description = "정산 완료 탭에서 보이는 목록입니다.")
-    public ResponseDto<SalaryDetailsResponse> getDoneSalary(@AuthenticationPrincipal User user) {
+    public ResponseEntity<ResponseDto<SalaryDetailsResponse>> getDoneSalary(@AuthenticationPrincipal User user) {
         SalaryDetailsResponse salary = salaryInfoUseCase.getSalaryDetail(user, true);
-        return ResponseDto.create(SALARY_FIND.getCode(), GET_SALARY_LIST_INFO.getMessage(), salary);
+        return ResponseEntity.ok(create(SALARY_FIND.getCode(), GET_SALARY_LIST_INFO.getMessage(), salary));
     }
 }
