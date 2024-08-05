8e56389d6cb3659afa6ec5e2940a3413910ba3e7
yang <ywj9811@gmail.com>
2024-08-06 04:23:20 +0900

    RAC-412 refactor : 관리자페이지 로직 분리

---
+++ src/main/java/com/postgraduate/admin/application/dto/res/SeniorInfoQuery.java
@@ -0,0 +1,8 @@
+package com.postgraduate.admin.application.dto.res;
+
+import com.postgraduate.domain.salary.domain.entity.Salary;
+import com.postgraduate.domain.wish.domain.entity.Wish;
+
+import java.util.Optional;
+
+public record SeniorInfoQuery(Salary salary, Optional<Wish> wish){}
