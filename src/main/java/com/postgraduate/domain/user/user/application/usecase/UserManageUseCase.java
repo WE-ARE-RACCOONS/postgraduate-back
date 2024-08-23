07bebe35da6046b6a6541391aa5d92118eced279
yang <ywj9811@gmail.com>
2024-08-24 06:36:17 +0900

    RAC-402 feat : 재가입 희망시 복구 로직 추가

--- src/main/java/com/postgraduate/domain/user/user/application/usecase/UserManageUseCase.java
+++ src/main/java/com/postgraduate/domain/user/user/application/usecase/UserManageUseCase.java
@@ -1,10 +1,16 @@
 package com.postgraduate.domain.user.user.application.usecase;
 
+import com.postgraduate.domain.auth.application.dto.req.RejoinRequest;
+import com.postgraduate.domain.auth.application.usecase.oauth.SelectOauth;
+import com.postgraduate.domain.auth.application.usecase.oauth.SignOutUseCase;
+import com.postgraduate.domain.auth.presentation.constant.Provider;
 import com.postgraduate.domain.user.user.application.dto.req.UserInfoRequest;
 import com.postgraduate.domain.user.user.application.utils.UserUtils;
 import com.postgraduate.domain.user.user.domain.entity.User;
 import com.postgraduate.domain.user.user.domain.service.UserGetService;
 import com.postgraduate.domain.user.user.domain.service.UserUpdateService;
+import com.postgraduate.domain.user.user.exception.DeletedUserException;
+import com.postgraduate.domain.user.user.exception.UserNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,6 +22,7 @@ public class UserManageUseCase {
     private final UserUpdateService userUpdateService;
     private final UserGetService userGetService;
     private final UserUtils userUtils;
+    private final SelectOauth selectOauth;
 
     public void updateInfo(User user, UserInfoRequest userInfoRequest) {
         userUtils.checkPhoneNumber(userInfoRequest.phoneNumber());
@@ -31,5 +38,18 @@ public class UserManageUseCase {
     @Transactional(readOnly = true)
     public Boolean duplicatedNickName(String nickName) {
         return userGetService.byNickName(nickName).isEmpty();
+    }
+
+    public User updateRejoin(Provider provider, RejoinRequest request) {
+        User user = userGetService.bySocialId(request.socialId());
+        if (!user.isDelete())
+            throw new UserNotFoundException();
+        if (!request.rejoin() || user.isRealDelete()) {
+            SignOutUseCase signOutUseCase = selectOauth.selectSignOut(provider);
+            signOutUseCase.reSignOut(request.socialId());
+            throw new DeletedUserException();
+        }
+        userUpdateService.updateRestore(user);
+        return user;
     }
 }
