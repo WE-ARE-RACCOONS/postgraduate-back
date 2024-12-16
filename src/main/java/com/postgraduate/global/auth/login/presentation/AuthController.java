633e5e8a0d5a1811c64a8d4331d32b77c286ea31
yang <ywj9811@gmail.com>
2024-12-17 07:11:11 +0900

    RAC-478 refactor : role 쿠키 추가

--- src/main/java/com/postgraduate/global/auth/login/presentation/AuthController.java
+++ src/main/java/com/postgraduate/global/auth/login/presentation/AuthController.java
@@ -18,6 +18,7 @@ import com.postgraduate.global.dto.ResponseDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -40,96 +41,97 @@ public class AuthController {
 
     @PostMapping("/login/token/{provider}")
     @Operation(summary = "소셜 로그인", description = "회원인 경우 JWT를, 회원이 아닌 경우 socialId를 반환합니다(회원가입은 진행하지 않습니다). 탈퇴 회원인 경우 isDelete = true 가 응답됩니다")
-    public ResponseEntity<ResponseDto<AuthResponse>> authLoginWithToken(@RequestBody @Valid TokenRequest request, @PathVariable Provider provider) {
+    public ResponseEntity<ResponseDto<AuthResponse>> authLoginWithToken(@RequestBody @Valid TokenRequest request, @PathVariable Provider provider, HttpServletResponse response) {
         SignInUseCase signInUseCase = selectOauth.selectSignIn(provider);
         AuthUserResponse authUser = signInUseCase.getUserWithToken(request);
         if (authUser.user() == null)
             return ResponseEntity.ok(create(AuthResponseCode.AUTH_NONE.getCode(), AuthResponseMessage.NOT_REGISTERED_USER.getMessage(), authUser));
-        JwtTokenResponse jwtToken = jwtUseCase.signIn(authUser.user());
+        JwtTokenResponse jwtToken = jwtUseCase.signIn(authUser.user(), response);
         return ResponseEntity.ok(create(AuthResponseCode.AUTH_ALREADY.getCode(), AuthResponseMessage.SUCCESS_AUTH.getMessage(), jwtToken));
     }
 
     @PostMapping("/login/{provider}")
     @Operation(summary = "소셜 로그인", description = "회원인 경우 JWT를, 회원이 아닌 경우 socialId를 반환합니다(회원가입은 진행하지 않습니다). 탈퇴 회원인 경우 isDelete = true 가 응답됩니다")
-    public ResponseEntity<ResponseDto<AuthResponse>> authLogin(@RequestBody @Valid CodeRequest request, @PathVariable Provider provider) {
+    public ResponseEntity<ResponseDto<AuthResponse>> authLogin(@RequestBody @Valid CodeRequest request, @PathVariable Provider provider, HttpServletResponse response) {
         SignInUseCase signInUseCase = selectOauth.selectSignIn(provider);
         AuthUserResponse authUser = signInUseCase.getUser(request);
         if (authUser.user() == null)
             return ResponseEntity.ok(create(AuthResponseCode.AUTH_NONE.getCode(), AuthResponseMessage.NOT_REGISTERED_USER.getMessage(), authUser));
-        JwtTokenResponse jwtToken = jwtUseCase.signIn(authUser.user());
+        JwtTokenResponse jwtToken = jwtUseCase.signIn(authUser.user(), response);
         return ResponseEntity.ok(create(AuthResponseCode.AUTH_ALREADY.getCode(), AuthResponseMessage.SUCCESS_AUTH.getMessage(), jwtToken));
     }
 
     @PostMapping("/dev/login/{provider}")
     @Operation(summary = "개발용 소셜 로그인", description = "회원인 경우 JWT를, 회원이 아닌 경우 socialId를 반환합니다(회원가입은 진행하지 않습니다). 탈퇴 회원인 경우 isDelete = true 가 응답됩니다")
-    public ResponseEntity<ResponseDto<AuthResponse>> authDevLogin(@RequestBody @Valid CodeRequest request, @PathVariable Provider provider) {
+    public ResponseEntity<ResponseDto<AuthResponse>> authDevLogin(@RequestBody @Valid CodeRequest request, @PathVariable Provider provider, HttpServletResponse response) {
         SignInUseCase signInUseCase = selectOauth.selectSignIn(provider);
         AuthUserResponse authUser = signInUseCase.getDevUser(request);
         if (authUser.user() == null)
             return ResponseEntity.ok(create(AuthResponseCode.AUTH_NONE.getCode(), AuthResponseMessage.NOT_REGISTERED_USER.getMessage(), authUser));
-        JwtTokenResponse jwtToken = jwtUseCase.signIn(authUser.user());
+        JwtTokenResponse jwtToken = jwtUseCase.signIn(authUser.user(), response);
         return ResponseEntity.ok(create(AuthResponseCode.AUTH_ALREADY.getCode(), AuthResponseMessage.SUCCESS_AUTH.getMessage(), jwtToken));
     }
 
     @PatchMapping("/rejoin/{provider}")
     @Operation(summary = "탈퇴 사용자 재가입", description = "복구를 희망하는 경우 rejoin = true 희망하지 않는 경우 false 를 넣어주세요. 복구시 기존 로그인과 동일한 응답")
-    public ResponseEntity<ResponseDto<AuthResponse>> reJoin(@PathVariable Provider provider, @RequestBody RejoinRequest request) {
+    public ResponseEntity<ResponseDto<AuthResponse>> reJoin(@PathVariable Provider provider, @RequestBody RejoinRequest request, HttpServletResponse response) {
         User user = userManageUseCase.updateRejoin(provider, request);
-        JwtTokenResponse jwtToken = jwtUseCase.signIn(user);
+        JwtTokenResponse jwtToken = jwtUseCase.signIn(user, response);
         return ResponseEntity.ok(create(AuthResponseCode.AUTH_ALREADY.getCode(), AuthResponseMessage.SUCCESS_AUTH.getMessage(), jwtToken));
     }
 
     @PostMapping("/logout")
     @Operation(summary = "로그아웃", description = "토큰 같이 보내주세요")
-    public ResponseEntity<ResponseDto<Void>> logout(@AuthenticationPrincipal User user) {
-        jwtUseCase.logout(user);
+    public ResponseEntity<ResponseDto<Void>> logout(@AuthenticationPrincipal User user, HttpServletResponse response) {
+        jwtUseCase.logout(user, response);
         return ResponseEntity.ok(create(AuthResponseCode.AUTH_DELETE.getCode(), AuthResponseMessage.LOGOUT_USER.getMessage()));
     }
 
     @PostMapping("/user/signup")
     @Operation(summary = "대학생 회원가입", description = "로그인 API에서 반환한 socialId, 닉네임, 번호, 마케팅 수신여부, 희망 학과, 희망 분야, 매칭 희망 여부")
-    public ResponseEntity<ResponseDto<JwtTokenResponse>> signUpUser(@RequestBody @Valid SignUpRequest request) {
+    public ResponseEntity<ResponseDto<JwtTokenResponse>> signUpUser(@RequestBody @Valid SignUpRequest request, HttpServletResponse response) {
         User user = signUpUseCase.userSignUp(request);
-        JwtTokenResponse jwtToken = jwtUseCase.signIn(user);
+        JwtTokenResponse jwtToken = jwtUseCase.signIn(user, response);
         return ResponseEntity.ok(create(AuthResponseCode.AUTH_CREATE.getCode(), AuthResponseMessage.SUCCESS_AUTH.getMessage(), jwtToken));
     }
 
     @PostMapping("/user/token")
     @Operation(summary = "후배로 변경 | 토큰 필요", description = "후배로 변경 가능한 경우 후배 토큰 발급")
-    public ResponseEntity<ResponseDto<JwtTokenResponse>> changeUserToken(@AuthenticationPrincipal User user) {
+    public ResponseEntity<ResponseDto<JwtTokenResponse>> changeUserToken(@AuthenticationPrincipal User user, HttpServletResponse response) {
         signUpUseCase.changeUser(user);
-        JwtTokenResponse jwtToken = jwtUseCase.changeUser(user);
+        JwtTokenResponse jwtToken = jwtUseCase.changeUser(user, response);
         return ResponseEntity.ok(create(AuthResponseCode.AUTH_CREATE.getCode(), AuthResponseMessage.SUCCESS_AUTH.getMessage(), jwtToken));
     }
 
     @PostMapping("/senior/signup")
     @Operation(summary = "대학원생 가입 - 필수 과정만", description = "대학원생 회원가입 - 필수 과정만")
-    public ResponseEntity<ResponseDto<JwtTokenResponse>> singUpSenior(@RequestBody @Valid SeniorSignUpRequest request) {
+    public ResponseEntity<ResponseDto<JwtTokenResponse>> singUpSenior(@RequestBody @Valid SeniorSignUpRequest request, HttpServletResponse response) {
         User user = signUpUseCase.seniorSignUp(request);
-        JwtTokenResponse jwtToken = jwtUseCase.signIn(user);
+        JwtTokenResponse jwtToken = jwtUseCase.signIn(user, response);
         return ResponseEntity.ok(create(SENIOR_CREATE.getCode(), CREATE_SENIOR.getMessage(), jwtToken));
     }
 
     @PostMapping("/senior/change")
     @Operation(summary = "선배로 추가 가입 | 토큰 필요", description = "대학생 대학원생으로 변경 추가 가입")
     public ResponseEntity<ResponseDto<JwtTokenResponse>> changeSenior(@AuthenticationPrincipal User user,
-                                                                      @RequestBody @Valid SeniorChangeRequest changeRequest) {
+                                                                      @RequestBody @Valid SeniorChangeRequest changeRequest,
+                                                                      HttpServletResponse response) {
         User changeUser = signUpUseCase.changeSenior(user, changeRequest);
-        JwtTokenResponse jwtToken = jwtUseCase.changeSenior(changeUser);
+        JwtTokenResponse jwtToken = jwtUseCase.changeSenior(changeUser, response);
         return ResponseEntity.ok(create(SENIOR_CREATE.getCode(), CREATE_SENIOR.getMessage(), jwtToken));
     }
 
     @PostMapping("/senior/token")
     @Operation(summary = "선배로 변경 | 토큰 필요", description = "선배로 변경 가능한 경우 선배 토큰 발급")
-    public ResponseEntity<ResponseDto<JwtTokenResponse>> changeSeniorToken(@AuthenticationPrincipal User user) {
-        JwtTokenResponse jwtToken = jwtUseCase.changeSenior(user);
+    public ResponseEntity<ResponseDto<JwtTokenResponse>> changeSeniorToken(@AuthenticationPrincipal User user, HttpServletResponse response) {
+        JwtTokenResponse jwtToken = jwtUseCase.changeSenior(user, response);
         return ResponseEntity.ok(create(AuthResponseCode.AUTH_CREATE.getCode(), AuthResponseMessage.SUCCESS_AUTH.getMessage(), jwtToken));
     }
 
     @PostMapping("/refresh")
     @Operation(summary = "토큰 재발급 | 토큰 필요", description = "refreshToken 으로 토큰 재발급")
-    public ResponseEntity<ResponseDto<JwtTokenResponse>> refresh(@AuthenticationPrincipal User user, HttpServletRequest request) {
-        JwtTokenResponse jwtToken = jwtUseCase.regenerateToken(user, request);
+    public ResponseEntity<ResponseDto<JwtTokenResponse>> refresh(@AuthenticationPrincipal User user, HttpServletRequest request, HttpServletResponse response) {
+        JwtTokenResponse jwtToken = jwtUseCase.regenerateToken(user, request, response);
         return ResponseEntity.ok(create(AuthResponseCode.AUTH_UPDATE.getCode(), AuthResponseMessage.SUCCESS_REGENERATE_TOKEN.getMessage(), jwtToken));
     }
 
